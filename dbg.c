9d7e17000c9df70235f5d1175218b407842f6c30
Landon Curt Noll <github-mail@asthe.com>
2021-12-23 14:10:45 -0800

    Added errp()

--- dbg.c
+++ dbg.c
@@ -1,5 +1,5 @@
 /*
- * dbg - example of how to use usage(), dbg(), warn(), err()
+ * dbg - example of how to use usage(), dbg(), warn(), err(), errp()
  *
  * We show how to use the usage(), dbg(), warn(), and err() facility by
  * this stub from an early verison of iocccsize.  Compile with -DDBG_TEST
@@ -57,10 +57,11 @@
  * Use the usage() function to print the usage message.
  */
 static char const *usage_msg =
-"usage: %s [-h] [-v level] work_dir iocccsize_path [tar_path]\n"
+"usage: %s [-h] [-v level] [-e errno] work_dir iocccsize_path [tar_path]\n"
 "\n"
 "\t-h\t\tprint help message and exit 0\n"
 "\t-v level\tset verbosity level: (def level: %d)\n"
+"\t-e errno\tsimilate setting of errno to cause errp() to be involed\n"
 "\n"
 "\twork_dir\tdirectory where the entry directory and tarball are formed\n"
 "\tiocccsize_path\tpath to the iocccsize tool\n"
@@ -93,6 +94,7 @@ static void usage(int exitcode, char const *name, char const *str);
 static void dbg(int level, char const *fmt, ...);
 static void warn(char const *name, char const *fmt, ...);
 static void err(int exitcode, char const *name, char const *fmt, ...);
+static void errp(int exitcode, char const *name, char const *fmt, ...);
 
 
 /*
@@ -312,7 +314,7 @@ err(int exitcode, char const *name, char const *fmt, ...)
     }
 
     /* issue the FATAL error */
-    ret = fprintf(stderr, "FATAL: %s: ", name);
+    ret = fprintf(stderr, "FATAL[%d]: %s: ", exitcode, name);
     if (ret < 0) {
 	warn(__FUNCTION__, "\nin err(%d, %s, %s ...): fprintf returned error: %d\n", exitcode, name, fmt, ret);
     }
@@ -337,19 +339,96 @@ err(int exitcode, char const *name, char const *fmt, ...)
 }
 
 
+/*
+ * errp - issue a fatal error message with errno information and exit
+ *
+ * given:
+ * 	exitcode	value to exit with
+ * 	name		name of function issuing the warning
+ * 	fmt		format of the warning
+ * 	...		optional format args
+ *
+ * This function does not return.
+ *
+ * Example:
+ *
+ * 	errp(1, __FUNCTION__, "bad foobar: %s", message);
+ *
+ * NOTE: We warn with extra newlines to help internal fault messages stand out.
+ *	 Normally one should NOT include newlines in warn messages.
+ */
+static void
+errp(int exitcode, char const *name, char const *fmt, ...)
+{
+    va_list ap;		/* argument pointer */
+    int ret;		/* return code holder */
+    int saved_errno;	/* errno value when called */
+
+    /* save errno in case we need it for strerror() */
+    saved_errno = errno;
+
+    /* start the var arg setup and fetch our first arg */
+    va_start(ap, fmt);
+
+    /* firewall */
+    if (exitcode < 0) {
+	warn(__FUNCTION__, "\nin err(): called with exitcode <0: %d\n", exitcode);
+	exitcode = 255;
+	warn(__FUNCTION__, "\nin err(): forcing exit code: %d\n", exitcode);
+    }
+    if (name == NULL) {
+	name = "((NULL name))";
+	warn(__FUNCTION__, "\nin err(): called with NULL name, forcing name: %s\n", name);
+    }
+    if (fmt == NULL) {
+	fmt = "((NULL fmt))";
+	warn(__FUNCTION__, "\nin err(): called with NULL fmt, forcing fmt: %s\n", fmt);
+    }
+
+    /* issue the FATAL error */
+    ret = fprintf(stderr, "FATAL[%d]: %s: ", exitcode, name);
+    if (ret < 0) {
+	warn(__FUNCTION__, "\nin err(%d, %s, %s ...): fprintf #0 returned error: %d\n", exitcode, name, fmt, ret);
+    }
+    ret = vfprintf(stderr, fmt, ap);
+    if (ret < 0) {
+	warn(__FUNCTION__, "\nin err(%d, %s, %s ...): vfprintf returned error: %d\n", exitcode, name, fmt, ret);
+    }
+    ret = fprintf(stderr, " errno[%d]: %s", saved_errno, strerror(saved_errno));
+    if (ret < 0) {
+	warn(__FUNCTION__, "\nin err(%d, %s, %s ...): fprintf #1  returned error: %d\n", exitcode, name, fmt, ret);
+    }
+    ret = fputc('\n', stderr);
+    if (ret != '\n') {
+	warn(__FUNCTION__, "\nin err(%d, %s, %s ...): fputc returned error: %d\n", exitcode, name, fmt, ret);
+    }
+    ret = fflush(stderr);
+    if (ret < 0) {
+	warn(__FUNCTION__, "\nin err(%d, %s, %s ...): fflush returned error: %d\n", exitcode, name, fmt, ret);
+    }
+
+    /* clean up stdarg stuff */
+    va_end(ap);
+
+    /* terminate */
+    exit(exitcode);
+}
+
+
 #if defined(DBG_TEST)
 int
 main(int argc, char *argv[])
 {
     extern char *optarg;		/* option argument */
     extern int optind;			/* argv index of the next arg */
+    int forced_errno;			/* -e errno setting */
     int i;
 
     /*
      * parse args
      */
     program = argv[0];
-    while ((i = getopt(argc, argv, "hv:")) != -1) {
+    while ((i = getopt(argc, argv, "hv:e:")) != -1) {
 	switch (i) {
 	case 'h':	/* -h - print help to stderr and exit 0 */
 	    usage(0, __FUNCTION__, "-h help mode:\n");
@@ -364,6 +443,17 @@ main(int argc, char *argv[])
 		err(1, __FUNCTION__, "cannot parse -v arg: %s error: %s", optarg, strerror(errno));
 		/*NOTREACHED*/
 	    }
+	    break;
+	case 'e':	/* -e errno - force errno */
+	    /* parse verbosity */
+	    errno = 0;
+	    forced_errno = strtol(optarg, NULL, 0);
+	    if (errno != 0) {
+		/* exit(1); */
+		err(1, __FUNCTION__, "cannot parse -v arg: %s error: %s", optarg, strerror(errno));
+		/*NOTREACHED*/
+	    }
+	    errno = forced_errno;	/* simulate errno setting */
 	    break;
 	default:
 	    usage(2, __FUNCTION__, "invalid -flag");
@@ -394,8 +484,12 @@ main(int argc, char *argv[])
     /*
      * simulate an error
      */
-    err(4, __FUNCTION__, "simulated error, work_dir: %s iocccsize_path: %s", work_dir, iocccsize_path);
-    /* exit(4); */
+    if (errno != 0) {
+	errp(4, __FUNCTION__, "simulated error, work_dir: %s iocccsize_path: %s", work_dir, iocccsize_path);
+	/* exit(4); */
+    }
+    err(5, __FUNCTION__, "simulated error, work_dir: %s iocccsize_path: %s", work_dir, iocccsize_path);
+    /* exit(5); */
     /*NOTREACHED*/
 
     /*
