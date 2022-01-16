7eceda54a0bc60fa6ad6ee23c558f013dfaa09c0
Landon Curt Noll <github-mail@asthe.com>
2022-01-16 01:53:34 -0800

    Fix after applying pull request #4
    Pull request #4: Fix compiler warning: type qualifiers ignored on function return type:
    
        https://github.com/ioccc-src/mkiocccentry/commit/d7729d1a0d09fa17db1b2469ed9ae2c08bf7d54a
    
    was made to address a compiler warning and to allow for use of -Wextra.
    
    We also fix the function prototype and add the -Wextra to the various Makefile options.

--- mkiocccentry.c
+++ mkiocccentry.c
@@ -765,7 +765,7 @@ static bool json_fprintf_value_long(FILE *stream, char const *lead, char const *
 				    char const *tail);
 static bool json_fprintf_value_bool(FILE *stream, char const *lead, char const *name, char const *middle, bool value,
 				    char const *tail);
-static char const * strnull(char const * const str);
+static char const * const strnull(char const * const str);
 static void write_info(struct info *infop, char const *entry_dir, bool test_mode);
 static void write_author(struct info *infop, int author_count, struct author *authorp, char const *entry_dir);
 static void form_tarball(char const *work_dir, char const *entry_dir, char const *tarball_path, char const *tar, char const *ls);
