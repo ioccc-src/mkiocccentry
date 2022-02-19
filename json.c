8c8aa80d7c2fdcd8116a1107eec783e932a4c31b
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2022-02-19 09:06:01 -0800

    Sequenced exit codes

--- json.c
+++ json.c
@@ -1753,10 +1753,10 @@ int check_common_json_fields(char const *file, char const *field, char const *va
 	errno = 0;
 	ts = strtol(value, NULL, 10);
 	if (errno != 0) {
-	    err(33, __func__, "unable to parse formed_timestamp \"%s\"", value);
+	    err(232, __func__, "unable to parse formed_timestamp \"%s\"", value);
 	    not_reached();
 	} else if (ts < MIN_TIMESTAMP) {
-	    err(34, __func__, "formed_timestamp '%ld' < MIN_TIMESTAMP '%ld'", ts, MIN_TIMESTAMP);
+	    err(233, __func__, "formed_timestamp '%ld' < MIN_TIMESTAMP '%ld'", ts, MIN_TIMESTAMP);
 	    not_reached();
 	}
     } else {
