91b9a0be73c8c13292a4e7d71b2615f34b4f929c
Landon Curt Noll <github-mail@asthe.com>
2022-02-21 13:45:26 -0800

    Adjusted limits
    MAX_TITLE_LEN: 20 => 24
    MAX_HANDLE: 24 => 32
    
    Adjusted whitespace padding in limit_ioccc.h
    and clarified remarks in a few comments.

--- limit_ioccc.h
+++ limit_ioccc.h
@@ -43,9 +43,10 @@
 
 #define RULE_2A_SIZE ((off_t)(4096))		/* Rule 2a size of prog.c */
 #define RULE_2B_SIZE ((size_t)(2503))		/* Rule 2b size of prog.c */
-#define MAX_TARBALL_LEN ((off_t)(3999971))	/* the compressed tarball formed cannot be longer than this many bytes */
-#define MAX_DIR_KSIZE (27651)			/* entry directory cannot exceed this size in kibibyte (1024 byte) blocks */
-#define MAX_ENTRY_NUM (9)		/* entry numbers from 0 to MAX_ENTRY_NUM - must be < 10 to the MAX_ENTRY_CHARS power */
+#define MAX_TARBALL_LEN ((off_t)(3999971))	/* compressed tarball size limit in bytes */
+/* NOTE: MAX_ENTRY_NUM must be < 10 to the MAX_ENTRY_CHARS power */
+#define MAX_DIR_KSIZE (27651)		/* entry directory size limit in kibibyte (1024 byte) blocks */
+#define MAX_ENTRY_NUM (9)		/* entry numbers from 0 to MAX_ENTRY_NUM */
 #define MAX_ENTRY_CHARS (1)		/* characters that represent the maximum entry number */
 #define MAX_AUTHORS (5)			/* maximum number of authors of an entry */
 #define MAX_NAME_LEN (48)		/* max author name length */
@@ -54,15 +55,15 @@
 #define MAX_TWITTER_LEN (19)		/* max twitter handle, including the leading @, length */
 #define MAX_GITHUB_LEN (16)		/* max GitHub account, including the leading @, length */
 #define MAX_AFFILIATION_LEN (48)	/* max affiliation name length */
-#define MAX_TITLE_LEN (20)		/* maximum length of a title */
+#define MAX_TITLE_LEN (32)		/* maximum length of a title */
 #define MAX_ABSTRACT_LEN (64)		/* maximum length of an abstract */
-#define MAX_HANDLE (24)		/* maximum IOCCC winner handle */
+#define MAX_HANDLE (32)			/* maximum IOCCC winner handle */
 #define MAX_BASENAME_LEN ((size_t)(99))	/* tar --format=v7 limits filenames to 99 characters */
-#define UUID_LEN (36)		/* characters in a UUID string - as per RFC4122 */
-#define UUID_VERSION (4)	/* version 4 - random UUID */
-#define UUID_VARIANT (0xa)	/* variant 1 - encoded as 0xa */
+#define UUID_LEN (36)			/* characters in a UUID string - as per RFC4122 */
+#define UUID_VERSION (4)		/* version 4 - random UUID */
+#define UUID_VARIANT (0xa)		/* variant 1 - encoded as 0xa */
 #define TIMESTAMP_EPOCH "Thu Jan 01 00:00:00 1970 UTC"	/* gettimeofday epoch */
-#define MAX_TIMESTAMP_LEN (48)    /* 28 + 20 more padding for locate */
+#define MAX_TIMESTAMP_LEN (48)		/* 28 + 20 more padding for locate */
 
 /*
  * Be careful to change this value as it will invalidate all IOCCC timestamps < this value
