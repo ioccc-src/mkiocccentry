3095bf2ae05a804633a150271c1c85e90a474bb4
Landon Curt Noll <github-mail@asthe.com>
2022-02-28 03:09:08 -0800

    Added -v level and -h args to jstr-test.sh

--- jstr-test.sh
+++ jstr-test.sh
@@ -2,6 +2,25 @@
 #
 # jstr-test.sh - JSON string encoding and decoding test
 
+# parse args
+#
+export V_FLAG="0"
+while getopts :hv: flag; do
+    case "$flag" in
+    h) echo "usage: $0 [-h] [-v level]" 1>&2
+       exit 2
+       ;;
+    v) V_FLAG="$OPTARG";
+       ;;
+    \?) echo "invalid option: -$OPTARG" 1>&2
+       exit 3
+       ;;
+    :) echo "option -$OPTARG requires an argument" 1>&2
+       exit 4
+       ;;
+    esac
+done
+
 # verify we have the tools and conditions to test
 #
 if [[ ! -x jstrencode ]]; then
@@ -28,7 +47,7 @@ trap "rm -f \$TEST_FILE \$TEST_FILE2; exit" 0 1 2 3 15
 # run the basic encoding test
 #
 echo "$0: about to run test #1"
-./jstrencode -t
+./jstrencode -v "$V_FLAG" -t
 status="$?"
 if [[ $status -eq 0 ]]; then
     echo "$0: test #1 passed"
@@ -40,9 +59,9 @@ fi
 # test JSON encoding and decoding pipe
 #
 echo "$0: about to run test #2"
-echo "./jstrencode -n < ./jstrencode | ./jstrdecode -n > $TEST_FILE"
+echo "./jstrencode -v $V_FLAG -n < ./jstrencode -v $V_FLAG | ./jstrdecode -v $V_FLAG -n > $TEST_FILE"
 # shellcheck disable=SC2094
-./jstrencode -n < ./jstrencode | ./jstrdecode -n > "$TEST_FILE"
+./jstrencode -v "$V_FLAG" -n < ./jstrencode -v "$V_FLAG" | ./jstrdecode -v "$V_FLAG" -n > "$TEST_FILE"
 if cmp -s ./jstrencode "$TEST_FILE"; then
     echo "$0: test #2 passed"
 else
@@ -50,9 +69,9 @@ else
     EXIT_CODE=2
 fi
 echo "$0: about to run test #3"
-echo "./jstrencode -n < ./jstrdecode | ./jstrdecode -n > $TEST_FILE"
+echo "./jstrencode -v $V_FLAG -n < ./jstrdecode -v $V_FLAG | ./jstrdecode -v $V_FLAG -n > $TEST_FILE"
 # shellcheck disable=SC2094
-./jstrencode -n < ./jstrdecode | ./jstrdecode -n > "$TEST_FILE"
+./jstrencode -v "$V_FLAG" -n < ./jstrdecode -v "$V_FLAG" | ./jstrdecode -v "$V_FLAG" -n > "$TEST_FILE"
 if cmp -s ./jstrdecode "$TEST_FILE"; then
     echo "$0: test #3 passed"
 else
@@ -63,9 +82,9 @@ fi
 # test JSON encoding and decoding pipe in strict mode
 #
 echo "$0: about to run test #4"
-echo "./jstrencode -n < ./jstrencode | ./jstrdecode -n -s > $TEST_FILE"
+echo "./jstrencode -v $V_FLAG -n < ./jstrencode -v $V_FLAG | ./jstrdecode -v $V_FLAG -n -s > $TEST_FILE"
 # shellcheck disable=SC2094
-./jstrencode -n < ./jstrencode | ./jstrdecode -n -s > "$TEST_FILE"
+./jstrencode -v "$V_FLAG" -n < ./jstrencode -v "$V_FLAG" | ./jstrdecode -v "$V_FLAG" -n -s > "$TEST_FILE"
 if cmp -s ./jstrencode "$TEST_FILE"; then
     echo "$0: test #4 passed"
 else
@@ -73,9 +92,9 @@ else
     EXIT_CODE=4
 fi
 echo "$0: about to run test #5"
-echo "./jstrencode -n < ./jstrdecode | ./jstrdecode -n -s > $TEST_FILE"
+echo "./jstrencode -v $V_FLAG -n < ./jstrdecode -v $V_FLAG | ./jstrdecode -v $V_FLAG -n -s > $TEST_FILE"
 # shellcheck disable=SC2094
-./jstrencode -n < ./jstrdecode | ./jstrdecode -n -s > "$TEST_FILE"
+./jstrencode -v "$V_FLAG" -n < ./jstrdecode -v "$V_FLAG" | ./jstrdecode -v "$V_FLAG" -n -s > "$TEST_FILE"
 if cmp -s ./jstrdecode "$TEST_FILE"; then
     echo "$0: test #5 passed"
 else
@@ -89,9 +108,9 @@ echo "$0: about to run test #6"
 export SRC_SET="jstr-test.sh dbg.c dbg.h fnamchk.c iocccsize.c jauthchk.c"
 SRC_SET="$SRC_SET jinfochk.c json.c json.h jstrdecode.c jstrencode.c"
 SRC_SET="$SRC_SET limit_ioccc.h mkiocccentry.c txzchk.c util.c util.h"
-echo "cat \$SRC_SET | ./jstrencode -n | ./jstrdecode -n > $TEST_FILE"
+echo "cat \$SRC_SET | ./jstrencode -v $V_FLAG -n | ./jstrdecode -v $V_FLAG -n > $TEST_FILE"
 # shellcheck disable=SC2086
-cat $SRC_SET | ./jstrencode -n | ./jstrdecode -n > "$TEST_FILE"
+cat $SRC_SET | ./jstrencode -v "$V_FLAG" -n | ./jstrdecode -v "$V_FLAG" -n > "$TEST_FILE"
 # shellcheck disable=SC2086
 cat $SRC_SET > "$TEST_FILE2"
 if cmp -s "$TEST_FILE2" "$TEST_FILE"; then
@@ -107,9 +126,9 @@ echo "$0: about to run test #7"
 export SRC_SET="jstr-test.sh dbg.c dbg.h fnamchk.c iocccsize.c jauthchk.c"
 SRC_SET="$SRC_SET jinfochk.c json.c json.h jstrdecode.c jstrencode.c"
 SRC_SET="$SRC_SET limit_ioccc.h mkiocccentry.c txzchk.c util.c util.h"
-echo "cat \$SRC_SET | ./jstrencode -n | ./jstrdecode -n -s > $TEST_FILE"
+echo "cat \$SRC_SET | ./jstrencode -v $V_FLAG -n | ./jstrdecode -v $V_FLAG -n -s > $TEST_FILE"
 # shellcheck disable=SC2086
-cat $SRC_SET | ./jstrencode -n | ./jstrdecode -n -s > "$TEST_FILE"
+cat $SRC_SET | ./jstrencode -v "$V_FLAG" -n | ./jstrdecode -v "$V_FLAG" -n -s > "$TEST_FILE"
 # shellcheck disable=SC2086
 cat $SRC_SET > "$TEST_FILE2"
 if cmp -s "$TEST_FILE2" "$TEST_FILE"; then
