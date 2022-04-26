63e40d8abf16bc17da4ce5a5ac580dec7d437e1f
Landon Curt Noll <github-mail@asthe.com>
2022-04-25 17:19:20 -0700

    Fixed test.sh and add test failure summary at the end
    Fixed some tests where the exit status was not being properly checked.
    
    If test(s) fail, a summary of the tests that failed is
    printed near the end of the run.

--- test.sh
+++ test.sh
@@ -51,7 +51,8 @@ Exit codes:
     >=20  some test failed"
 export TEST_VERSION="0.462022-04-23"
 export V_FLAG="0"
-EXIT_CODE="0"
+export EXIT_CODE="0"
+export FAILURE_SUMMARY=
 
 # parse args
 #
@@ -195,6 +196,8 @@ echo "./iocccsize-test.sh -v 1"
 status="$?"
 if [[ $status -ne 0 ]]; then
     echo "$0: ERROR: iocccsize-test.sh non-zero exit code: $status" 1>&2
+    FAILURE_SUMMARY="$FAILURE_SUMMARY
+    iocccsize-test.sh non-zero exit code: $status"
     EXIT_CODE="20"
 else
     echo
@@ -212,6 +215,8 @@ echo "./dbg -e 2 foo bar baz >dbg.out 2>&1"
 status="$?"
 if [[ $status -ne 5 ]]; then
     echo "exit status of dbg: $$status != 5";
+    FAILURE_SUMMARY="$FAILURE_SUMMARY
+    exit status of dbg: $status != 5";
     EXIT_CODE="21"
 else
     grep -q '^FATAL\[5\]: main: simulated error, foo: foo bar: bar errno\[2\]: No such file or directory$' dbg.out
@@ -221,6 +226,8 @@ else
 	echo "$0: ERROR: beginning dbg.out contents" 1>&2
 	cat dbg.out 1>&2
 	echo "$0: ERROR: dbg.out contents complete" 1>&2
+	FAILURE_SUMMARY="$FAILURE_SUMMARY
+			 did not find the correct dbg error message"
 	EXIT_CODE="22"
     else
 	echo
@@ -239,6 +246,8 @@ echo "./mkiocccentry-test.sh"
 status="$?"
 if [[ $status -ne 0 ]]; then
     echo "$0: ERROR: mkiocccentry-test.sh non-zero exit code: $status" 1>&2
+    FAILURE_SUMMARY="$FAILURE_SUMMARY
+    mkiocccentry-test.sh non-zero exit code: $status"
     EXIT_CODE="23"
 else
     echo
@@ -253,8 +262,11 @@ echo "RUNNING: jstr-test.sh"
 echo
 echo "./jstr-test.sh"
 ./jstr-test.sh
+status="$?"
 if [[ $status -ne 0 ]]; then
     echo "$0: ERROR: jstr-test.sh non-zero exit code: $status" 1>&2
+    FAILURE_SUMMARY="$FAILURE_SUMMARY
+    jstr-test.sh non-zero exit code: $status"
     EXIT_CODE="24"
 else
     echo
@@ -268,8 +280,11 @@ echo "RUNNING: jint -t"
 echo
 echo "./jint -t"
 ./jint -t
+status="$?"
 if [[ $status -ne 0 ]]; then
     echo "$0: ERROR: jint non-zero exit code: $status" 1>&2
+    FAILURE_SUMMARY="$FAILURE_SUMMARY
+    jint non-zero exit code: $status"
     #
     # XXX - Both ./jint and ./jfloat will be replaced by ./jnumber
     # XXX	So we use the same 'exit 7' code for both of them.
@@ -287,8 +302,11 @@ echo "RUNNING: jfloat -t"
 echo
 echo "./jfloat -t"
 ./jfloat -t
+status="$?"
 if [[ $status -ne 0 ]]; then
     echo "$0: ERROR: jfloat non-zero exit code: $status" 1>&2
+    FAILURE_SUMMARY="$FAILURE_SUMMARY
+    jfloat non-zero exit code: $status"
     #
     # XXX - Both ./jint and ./jfloat will be replaced by ./jnumber
     # XXX	So we use the same 'exit 7' code for both of them.
@@ -306,8 +324,11 @@ echo "RUNNING: dyn_test"
 echo
 echo "./dyn_test"
 ./dyn_test
+status="$?"
 if [[ $status -ne 0 ]]; then
     echo "$0: ERROR: dyn_test non-zero exit code: $status" 1>&2
+    FAILURE_SUMMARY="$FAILURE_SUMMARY
+    dyn_test non-zero exit code: $status"
     EXIT_CODE="26"
 else
     echo
@@ -322,7 +343,8 @@ if [[ $EXIT_CODE -eq 0 ]]; then
     echo
 else
     echo
-    echo "Some test(s) failed"
+    echo "These test(s) failed:"
+    echo "$FAILURE_SUMMARY"
     echo
     echo "About to exit: $EXIT_CODE"
     echo
