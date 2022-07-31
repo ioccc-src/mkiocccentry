64897424ebbbc8298b0caa740119f6b1b1827c35
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2022-07-31 09:21:46 -0700

    Add to commit d643720d09b8c9991da8dbc9d32bcf8c8a9

--- chkentry.h
+++ chkentry.h
@@ -140,6 +140,48 @@ static char const *Bfgrexbeo[] =
     "uhznaf jnf gurve unovg bs pbagvahnyyl fgngvat naq ercrngvat gur irel irel\n"
     "boivbhf.\n\nBar bs gur guvatf Sbeq Cersrpg unq nyjnlf sbhaq uneqrfg gb haqrefgnaq nobhg\n"
     "uhznaf jnf gurve unovg bs pbagvahnyyl fgngvat naq ercrngvat gur irel irel\nboivbhf.",
+    "Sbe n zbzrag, abguvat unccrarq. Gura, nsgre n frpbaq be fb, abguvat\npbagvahrq gb unccra.",
+    "'Vs V rire zrrg zlfrys,' fnvq Mncubq, 'V'yy uvg zlfrys fb uneq V jba'g xabj\njung'f uvg zr.'",
+    "'V frr abobql ba gur ebnq,' fnvq Nyvpr.\n\n'V bayl jvfu gung V unq fhpu rlrf,' gur Xvat erznexrq"
+    " va n sergshy\ngbar. 'Gb or noyr gb frr Abobql! Naq ng gung qvfgnapr gbb! Jul, vg'f\nnf zhpu nf V"
+    " pna qb gb frr erny crbcyr, ol guvf yvtug!'\n\n[...]\n\n'Jub qvq lbh cnff ba gur ebnq?' gur Xvat jrag"
+    " ba, ubyqvat bhg uvf\nunaq gb gur Zrffratre sbe fbzr zber unl.\n\n'Abobql,' fnvq gur Zrffratre.\n\n'"
+    "Dhvgr evtug,' fnvq gur Xvat: 'guvf lbhat ynql fnj uvz gbb. Fb bs pbhefr Abobql\njnyxf fybjre guna lbh.'\n\n"
+    "'V qb zl orfg,' gur Zrffratre fnvq va n fhyxl gbar. 'V'z fher abobql jnyxf zhpu\nsnfgre guna V qb!'\n\n"
+    "'Ur pna'g qb gung,' fnvq gur Xvat, 'be ryfr ur'q unir orra urer svefg.'",
+    "'Guvf vf n puvyq!' Unvtun ercyvrq rntreyl, pbzvat va sebag bs Nyvpr gb\n"
+    "vagebqhpr ure, naq fcernqvat bhg obgu uvf unaqf gbjneqf ure va na Natyb-Fnkba\n"
+    "nggvghqr. 'Jr bayl sbhaq vg gb-qnl. Vg'f nf ynetr nf yvsr, naq gjvpr nf\nangheny!'\n\n"
+    "'V nyjnlf gubhtug gurl jrer snohybhf zbafgref!' fnvq gur Havpbea. 'Vf vg\nnyvir?'\n\n'"
+    "Vg pna gnyx,' fnvq Unvtun, fbyrzayl.\nGur Havpbea ybbxrq qernzvyl ng Nyvpr, naq fnvq 'Gnyx, puvyq.'\n"
+    "Nyvpr pbhyq abg uryc ure yvcf pheyvat hc vagb n fzvyr nf fur ortna: 'Qb lbh\nxabj, V nyjnlf gubhtug"
+    " Havpbeaf jrer snohybhf zbafgref, gbb! V arire fnj bar\nnyvir orsber!'\n\n[...]\n\n'Nu, jung vf vg, abj?'"
+    " gur Havpbea pevrq rntreyl. 'Lbh'yy arire thrff! V\npbhyqa'g.' Gur Yvba ybbxrq ng Nyvpr jrnevyl. 'Ner lbh"
+    " na navzny -- irtrgnoyr --\nbe zvareny?' ur fnvq, lnjavat ng rirel bgure jbeq.\n\n'Vg'f n snohybhf zbafgre!'"
+    " gur Havpbea pevrq bhg, orsber Nyvpr pbhyq ercyl.\n\n'Gura unaq ebhaq gur cyhz-pnxr, Zbafgre,' gur Yvba fnvq.",
+    "V ortna gb unir qbhogf nobhg ubj zngu vf gnhtug va guvf pbhagel jura n lbhat\n"
+    "pbbx ng zl cvmmrevn fnvq ur unq n ceboyrz. 'N phfgbzre pnyyrq, nfxvat sbe n\ncvr gung'f n"
+    " guveq purrfr, n guveq crccrebav, naq n guveq pbzovangvba,' ur\nfnvq. 'Fb, jung'f gur ceboyrz?' "
+    "V nfxrq uvz. 'V qba'g xabj jung gb chg ba\ngur sbhegu guveq'.\n\n-EBOREG XRNEARL\nEnapub Pbeqbin,\nPnyvsbeavn",
+    "'Naq lbh qb Nqqvgvba?' gur Juvgr Dhrra nfxrq. 'Jung'f bar naq bar naq bar naq\nbar naq bar naq bar"
+    " naq bar naq bar naq bar naq bar?' 'V qba'g xabj,' fnvq\nNyvpr. 'V ybfg pbhag.'\n\n'Fur pna'g qb Nqqvgvba,'"
+    " gur Erq Dhrra vagreehcgrq. 'Pna lbh qb Fhogenpgvba?'\nGnxr avar sebz rvtug.' 'Avar sebz rvtug V pna'g, lbh xabj,'"
+    " Nyvpr ercyvrq irel\nernqvyl: 'ohg--'\n\n'Fur pna'g qb Fhogenpgvba,' fnvq gur Juvgr Dhrra. 'Pna lbh qb Qvivfvba? "
+    "Qvivqr\nn ybns ol n xavsr -- jung'f gur nafjre gb gung?' 'V fhccbfr--' Nyvpr jnf\nortvaavat, ohg gur Erq Dhrra "
+    "nafjrerq sbe ure.  'Oernq-naq-ohggre, bs pbhefr.\nGel nabgure Fhogenpgvba fhz. Gnxr n obar sebz n qbt: jung erznvaf?'\n\n"
+    "Nyvpr pbafvqrerq. 'Gur obar jbhyqa'g erznva, bs pbhefr, vs V gbbx vg--naq gur\nqbt jbhyqa'g erznva; vg jbhyq pbzr gb ovgr "
+    "zr--naq V'z fher V fubhyqa'g\nerznva!' 'Gura lbh guvax abguvat jbhyq erznva?' fnvq gur Erq Dhrra.\n\n'V guvax gung'f gur "
+    "nafjre.' 'Jebat, nf hfhny,' fnvq gur Erq Dhrra: 'gur qbt'f\ngrzcre jbhyq erznva.' 'Ohg V qba'g frr ubj --'\n\n"
+    "'Jul, ybbx urer!' gur Erq Dhrra pevrq. 'Gur qbt jbhyq ybfr vgf grzcre, jbhyqa'g\nvg?'\n\n"
+    "'Creuncf vg jbhyq,' Nyvpr ercyvrq pnhgvbhfyl. 'Gura vs gur qbt jrag njnl, vgf\ngrzcre jbhyq erznva!' "
+    "gur Dhrra rkpynvzrq gevhzcunagyl.",
+    "Urer gur Erq Dhrra ortna ntnva. 'Pna lbh nafjre hfrshy dhrfgvbaf?' fur fnvq.\n"
+    "'Ubj vf oernq znqr?'\n\n'V xabj gung!' Nyvpr pevrq rntreyl.' Lbh gnxr fbzr sybhe--'\n"
+    "'Jurer qb lbh cvpx gur sybjre?' gur Juvgr Dhrra nfxrq. 'Va n tneqra, be va gur\nurqtrf?'\n\n"
+    "'Jryy vg vfa'g cvpxrq ng nyy,' Nyvpr rkcynvarq: 'vg'f tebhaq--'\n'Ubj znal nperf bs tebhaq?' "
+    "fnvq gur Juvgr Dhrra. 'Lbh zhfga'g yrnir bhg fb\nznal guvatf.'\n\n'Sna ure urnq!' gur Erq Dhrra "
+    "nakvbhfyl vagreehcgrq. 'Fur'yy or srirevfu nsgre\nfb zhpu guvaxvat.' Fb gurl frg gb jbex naq snaarq "
+    "ure jvgu ohapurf bs yrnirf,\ngvyy fur unq gb ort gurz gb yrnir bss, vg oyrj ure unve nobhg fb.",
     NULL
 };
 
