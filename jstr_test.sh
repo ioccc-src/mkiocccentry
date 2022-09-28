90024707f1baf52e288c6d4542e7c36f9c2407c8
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2022-09-28 03:18:26 -0700

    Improve and bug fix jstr_test.sh
    Now allows for specifying path to jstrencode and jstrdecode. The usage
    looks like:
    
        usage: ./jstr_test.sh [-h] [-v level] [-e jstrencode] [-d jstrdecode]
    
    	-h		print help and exit 1
    	-v		set verbosity level for this script (def level: 0)
    	-e		path to jstrencode tool (def: ./jstrencode)
    	-d		path to jstrdecode tool (def: ./jstrdecode)
    
        exit codes:
    
    	0	    - all is well
    	1	    - -h used
    	2	    - invalid option
    	3	    - option argument missing
    	42	    - test #0 failed
    	43	    - test #1 failed
    	44	    - test #2 failed
    	45	    - test #3 failed
    	100	    - jstrencode not executable
    	101	    - jstrdecode not executable
    	102	    - jstr_test.out exists and can't remove it
    	103	    - jstr_test2.out exists and can't remove it
    
    The script first tries removing the test files and if they exist after
    that then it fails whereas previously it was a fatal error if they
    existed (saying to move or remove them).
    
    At least two exit codes were used for more than one purpose - in getopts
    parsing and also as a test failure code. This has been rectified.
    
    The exit codes have been changed a bit so that more tests could be added
    if necessary (or any other category of exit codes). The command line /
    getopts codes currently are 1 - 3; the exit codes for test failures are
    42 - 45 and the checks prior to running the tests are 100 - 103.
    
    Passes shellcheck.
    
    The man page still needs to be created and I plan on doing this in the
    near future - possibly today but more likely it won't be.

--- jstr_test.sh
+++ jstr_test.sh
@@ -2,21 +2,52 @@
 #
 # jstr_test.sh - JSON string encoding and decoding test
 
+export JSTRENCODE="./jstrencode"
+export JSTRDECODE="./jstrdecode"
+export TEST_FILE="jstr_test.out"
+export TEST_FILE2="jstr_test2.out"
+
+export USAGE="usage: $0 [-h] [-v level] [-e jstrencode] [-d jstrdecode]
+
+    -h		print help and exit 1
+    -v		set verbosity level for this script (def level: 0)
+    -e		path to jstrencode tool (def: $JSTRENCODE)
+    -d		path to jstrdecode tool (def: $JSTRDECODE)
+
+exit codes:
+
+    0	    - all is well
+    1	    - -h used
+    2	    - invalid option
+    3	    - option argument missing
+    42	    - test #0 failed
+    43	    - test #1 failed
+    44	    - test #2 failed
+    45	    - test #3 failed
+    100	    - jstrencode not executable
+    101	    - jstrdecode not executable
+    102	    - $TEST_FILE exists and can't remove it
+    103	    - $TEST_FILE2 exists and can't remove it
+"
 # parse args
 #
 export V_FLAG="0"
-while getopts :hv: flag; do
+while getopts :hv:e:d: flag; do
     case "$flag" in
-    h) echo "usage: $0 [-h] [-v level]" 1>&2
-       exit 2
+    h) echo "$USAGE" 1>&2
+       exit 1
        ;;
     v) V_FLAG="$OPTARG";
        ;;
+    e) JSTRENCODE="$OPTARG"
+       ;;
+    d) JSTRDECODE="$OPTARG"
+       ;;
     \?) echo "invalid option: -$OPTARG" 1>&2
-       exit 3
+       exit 2
        ;;
     :) echo "option -$OPTARG requires an argument" 1>&2
-       exit 4
+       exit 3
        ;;
    *)
        ;;
@@ -27,16 +58,19 @@ eval make all 2>&1 | grep -v 'Nothing to be done for'
 
 # verify we have the tools and conditions to test
 #
-if [[ ! -x jstrencode ]]; then
-    echo "$0: missing jstrencode tool" 1>&2
+if [[ ! -x "$JSTRENCODE" ]]; then
+    echo "$0: missing jstrencode tool: $JSTRENCODE" 1>&2
     exit 100
 fi
-if [[ ! -x jstrdecode ]]; then
-    echo "$0: missing jstrdecode tool" 1>&2
+if [[ ! -x "$JSTRDECODE" ]]; then
+    echo "$0: missing jstrdecode tool: $JSTRDECODE" 1>&2
     exit 101
 fi
-export TEST_FILE="jstr_test.out"
-export TEST_FILE2="jstr_test2.out"
+# try removing the test files first
+echo rm -f "$TEST_FILE $TEST_FILE2"
+rm -f "$TEST_FILE $TEST_FILE2"
+
+# if the files still exist let the user know and don't proceed
 if [[ -e "$TEST_FILE" ]]; then
     echo "$0: found $TEST_FILE; move or remove before running this test" 1>&2
     exit 102
@@ -50,56 +84,56 @@ trap "rm -f \$TEST_FILE \$TEST_FILE2; exit" 0 1 2 3 15
 
 # run the basic encoding test
 #
-echo "$0: about to run test #1"
-./jstrencode -v "$V_FLAG" -t
+echo "$0: about to run test #0"
+"$JSTRENCODE" -v "$V_FLAG" -t
 status="$?"
 if [[ $status -eq 0 ]]; then
-    echo "$0: test #1 passed"
+    echo "$0: test #0 passed"
 else
-    echo "$0: test #1 failed" 1>&2
-    EXIT_CODE=1
+    echo "$0: test #0 failed" 1>&2
+    EXIT_CODE=42
 fi
 
 # test JSON encoding and decoding pipe
 #
-echo "$0: about to run test #2"
-echo "./jstrencode -v $V_FLAG -n < ./jstrencode -v $V_FLAG | ./jstrdecode -v $V_FLAG -n > $TEST_FILE"
+echo "$0: about to run test #1"
+echo "$JSTRENCODE -v $V_FLAG -n < $JSTRENCODE -v $V_FLAG | ./jstrdecode -v $V_FLAG -n > $TEST_FILE"
 # shellcheck disable=SC2094
-./jstrencode -v "$V_FLAG" -n < ./jstrencode -v "$V_FLAG" | ./jstrdecode -v "$V_FLAG" -n > "$TEST_FILE"
-if cmp -s ./jstrencode "$TEST_FILE"; then
+"$JSTRENCODE" -v "$V_FLAG" -n < "$JSTRENCODE" -v "$V_FLAG" | ./jstrdecode -v "$V_FLAG" -n > "$TEST_FILE"
+if cmp -s "$JSTRENCODE" "$TEST_FILE"; then
+    echo "$0: test #1 passed"
+else
+    echo "$0: test #1 failed" 1>&2
+    EXIT_CODE=43
+fi
+echo "$0: about to run test #2"
+echo "$JSTRENCODE -v $V_FLAG -n < $JSTRDECODE -v $V_FLAG | $JSTRDECODE -v $V_FLAG -n > $TEST_FILE"
+# shellcheck disable=SC2094
+"$JSTRENCODE" -v "$V_FLAG" -n < "$JSTRDECODE" -v "$V_FLAG" | "$JSTRDECODE" -v "$V_FLAG" -n > "$TEST_FILE"
+if cmp -s "$JSTRDECODE" "$TEST_FILE"; then
     echo "$0: test #2 passed"
 else
     echo "$0: test #2 failed" 1>&2
-    EXIT_CODE=2
-fi
-echo "$0: about to run test #3"
-echo "./jstrencode -v $V_FLAG -n < ./jstrdecode -v $V_FLAG | ./jstrdecode -v $V_FLAG -n > $TEST_FILE"
-# shellcheck disable=SC2094
-./jstrencode -v "$V_FLAG" -n < ./jstrdecode -v "$V_FLAG" | ./jstrdecode -v "$V_FLAG" -n > "$TEST_FILE"
-if cmp -s ./jstrdecode "$TEST_FILE"; then
-    echo "$0: test #3 passed"
-else
-    echo "$0: test #3 failed" 1>&2
-    EXIT_CODE=3
+    EXIT_CODE=44
 fi
 
 # test some text holes in the encoding and decoding pipe
 #
-echo "$0: about to run test #6"
+echo "$0: about to run test #3"
 export SRC_SET="jstr_test.sh dbg.c dbg.h fnamchk.c iocccsize.c"
 SRC_SET="$SRC_SET chkentry.c json_parse.c json_parse.h jstrdecode.c jstrencode.c"
 SRC_SET="$SRC_SET limit_ioccc.h mkiocccentry.c txzchk.c util.c util.h"
-echo "cat \$SRC_SET | ./jstrencode -v $V_FLAG -n | ./jstrdecode -v $V_FLAG -n > $TEST_FILE"
+echo "cat \$SRC_SET | $JSTRENCODE -v $V_FLAG -n | $JSTRDECODE -v $V_FLAG -n > $TEST_FILE"
 # shellcheck disable=SC2086
 # shellcheck disable=SC2002
-cat $SRC_SET | ./jstrencode -v "$V_FLAG" -n | ./jstrdecode -v "$V_FLAG" -n > "$TEST_FILE"
+cat $SRC_SET | $JSTRENCODE -v "$V_FLAG" -n | $JSTRDECODE -v "$V_FLAG" -n > "$TEST_FILE"
 # shellcheck disable=SC2086
 cat $SRC_SET > "$TEST_FILE2"
 if cmp -s "$TEST_FILE2" "$TEST_FILE"; then
-    echo "$0: test #6 passed"
+    echo "$0: test #3 passed"
 else
-    echo "$0: test #6 failed" 1>&2
-    EXIT_CODE=6
+    echo "$0: test #3 failed" 1>&2
+    EXIT_CODE=45
 fi
 
 # All Done!!! -- Jessica Noll, Age 2
