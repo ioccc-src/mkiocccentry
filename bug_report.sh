8d949c7b7d2c1ec791c4145f3d845db115d86dee
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2022-10-13 09:28:19 -0700

    Update bug_report.sh - initial code
    The script is still quite basic but it now creates a log file, reporting
    the name if -v > 1 (and always at the end of the script). It then runs,
    noting each step in the log file as well as stdout, make clobber, make
    all and make test.
    
    If any step fails it changes the exit code and notes this in the log
    file as well. At the end if the exit code is not 0 it suggests the user
    report an issue and attach the log file. Otherwise it tells the user
    that no test fails and lets them know that they can safely delete the
    log file if they decide to.
    
    The log file name is calculated like:
    
        LOG_FILE="bug-report.$(/bin/date +%Y%m%d.%H%M%S).txt"
    
    and the reason it doesn't start with bug_report is to distinguish it
    from the script itself.
    
    Added to .gitignore bug-report*.txt.
    
    A lot more has to be added but this is a good start. It's possible I
    will add more today but that's not certain. For now I am going to look
    at the comments on the GitHub issue page and after replying I'll see how
    I feel.

--- bug_report.sh
+++ bug_report.sh
@@ -3,6 +3,109 @@
 # bug-report.sh - collect system information to help user report bugs and issues
 # using the mkiocccentry tools
 #
-# NOTE: This is a work in progress. Right now it's just a stub.
+# NOTE: This is a work in progress.
 
-exit 0
+export BUG_REPORT_VERSION="0.0 2022-10-13"
+export USAGE="usage: $0 [-h] [-V] [-v level]
+
+    -h			    print help and exit 2
+    -V			    print version and exit 2
+    -v level		    set verbosity level for this script: (def level: 0)
+
+exit codes:
+    0 - all is well
+    1 - failed to create a bug report file
+    2 - help mode exit or print version mode exit
+    3 - invalid command line
+    >= 10 - internal error
+
+$0 version: $BUG_REPORT_VERSION"
+
+
+# parse args
+#
+export V_FLAG="0"
+while getopts :hVv:t:d:T:F: flag; do
+    case "$flag" in
+    h)	echo "$USAGE" 1>&2
+	exit 2
+	;;
+    V)	echo "$BUG_REPORT_VERSION" 1>&2
+	exit 2
+	;;
+    v)	V_FLAG="$OPTARG";
+	;;
+    \?) echo "$0: ERROR: invalid option: -$OPTARG" 1>&2
+	exit 3
+	;;
+    :)	echo "$0: ERROR: option -$OPTARG requires an argument" 1>&2
+	exit 3
+	;;
+   *)
+	;;
+    esac
+done
+
+
+# assume all tests will pass
+EXIT_CODE=0
+
+# NOTE: log file does not have an underscore in the name because we want to
+# distinguish it from this script which does have an underscore in it.
+LOG_FILE="bug-report.$(/bin/date +%Y%m%d.%H%M%S).txt"
+
+# attempt to create log file
+rm -f "$LOG_FILE"
+touch "$LOG_FILE"
+
+if [[ ! -e "$LOG_FILE" ]]; then
+    echo "$0: could not create log file: $LOG_FILE"
+    exit 1
+fi
+if [[ ! -w "$LOG_FILE" ]]; then
+    echo "$0: log file not writable: $LOG_FILE"
+    exit 1
+fi
+
+if [[ $V_FLAG -gt 1 ]]; then
+    echo "Will write contents to $LOG_FILE" 1>&2
+fi
+
+echo "## TIME OF REPORT: $(date)" | tee -a -- "$LOG_FILE"
+echo "## RUNNING make clobber: " | tee -a -- "$LOG_FILE"
+make clobber | tee -a -- "$LOG_FILE"  1>&2
+status=$?
+if [[ "$status" -ne 0 ]]; then
+    EXIT_CODE=10
+    echo "make clobber failed: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
+fi
+echo '' >> "$LOG_FILE" 1>&2
+echo "## RUNNING make all: " | tee -a -- "$LOG_FILE" 1>&2
+make all | tee -a -- "$LOG_FILE"  1>&2
+status=$?
+if [[ "$status" -ne 0 ]]; then
+    EXIT_CODE=11
+    echo "make all failed: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
+fi
+echo '' >> "$LOG_FILE" 1>&2
+echo "## RUNNING make test: " | tee -a -- "$LOG_FILE" 1>&2
+make test | tee -a -- "$LOG_FILE"  1>&2
+status=$?
+if [[ "$status" -ne 0 ]]; then
+    EXIT_CODE=12
+    echo "make test failed with exit code $status: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
+fi
+
+if [[ "$EXIT_CODE" -ne 0 ]]; then
+    echo "Found one more issues. Please file an issue on the GitHub issues page and attach" 1>&2
+    echo "the file $LOG_FILE in your report" 1>&2
+else
+    echo "All tests PASSED" 1>&2
+    echo "A log of the above tests was saved to $LOG_FILE" 1>&2
+    echo "If you feel everything is in order you may safely delete the file." 1>&2
+fi
+
+if [[ "$V_FLAG" -gt 1 ]]; then
+    echo "About to exit with exit code: $EXIT_CODE" 1>&2
+fi
+exit "$EXIT_CODE"
