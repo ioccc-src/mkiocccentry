8343c4b8cb97e52df64fe8973e68f0d83c6090e1
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2022-10-13 11:43:46 -0700

    Add initial code of bug_report.sh
    The script is still quite basic but it now creates a log file, reporting
    the name if -v > 1 (and always at the end of the script). It then runs,
    noting each step in the log file as well as stdout, make clobber, make
    all and make test. It also runs hostchk.sh.
    
    If any step fails it changes the exit code and notes this in the log
    file as well (but see below). At the end if the exit code is not 0 it
    suggests the user report an issue and attach the log file. Otherwise it
    tells the user that no test fails and lets them know that they can
    safely delete the log file if they decide to.
    
    The log file name is calculated like:
    
        LOG_FILE="bug-report.$(/bin/date +%Y%m%d.%H%M%S).txt"
    
    and the reason it doesn't start with bug_report is to distinguish it
    from the script itself.
    
    Added to .gitignore bug-report*.txt.
    
    There is also a note of a bug in the script with a FIXME note.
    
    And no: amusing as it would have been I cannot say this was intentional
    :-) I did think of it earlier on but I forgot to address it and this
    will not be fixed until another date. Still it's probably good that it's
    there because we can all use some amusement and laughs. Anyway the
    problem is that the exit codes will not be tested right which makes
    checking for them entirely useless.
    
    The Makefile has been updated so that make shellcheck will check the
    script. Also changed the name to bug_report.sh to match the style of the
    rest of the files (some of which used to have - but were changed to
    underscore).

---
+++ bug_report.sh
@@ -0,0 +1,135 @@
+#!/usr/bin/env bash
+#
+# bug-report.sh - collect system information to help user report bugs and issues
+# using the mkiocccentry tools
+#
+# NOTE: This is a work in progress.
+
+export BUG_REPORT_VERSION="0.0 2022-10-13"
+export USAGE="usage: $0 [-h] [-V] [-v level]
+
+    -h			    print help and exit 2
+    -V			    print version and exit 2
+    -v level		    set verbosity level for this script: (def level: 0)
+
+exit codes:
+    0 - all is well
+    1 - failed to create a bug report file
+    2 - help mode exit or print version mode exit
+    3 - invalid command line
+    >= 10 - internal error
+
+$0 version: $BUG_REPORT_VERSION"
+
+
+# parse args
+#
+export V_FLAG="0"
+while getopts :hVv:t:d:T:F: flag; do
+    case "$flag" in
+    h)	echo "$USAGE" 1>&2
+	exit 2
+	;;
+    V)	echo "$BUG_REPORT_VERSION" 1>&2
+	exit 2
+	;;
+    v)	V_FLAG="$OPTARG";
+	;;
+    \?) echo "$0: ERROR: invalid option: -$OPTARG" 1>&2
+	exit 3
+	;;
+    :)	echo "$0: ERROR: option -$OPTARG requires an argument" 1>&2
+	exit 3
+	;;
+   *)
+	;;
+    esac
+done
+
+
+# assume all tests will pass
+EXIT_CODE=0
+
+# NOTE: log file does not have an underscore in the name because we want to
+# distinguish it from this script which does have an underscore in it.
+LOG_FILE="bug-report.$(/bin/date +%Y%m%d.%H%M%S).txt"
+
+# attempt to create log file
+rm -f "$LOG_FILE"
+touch "$LOG_FILE"
+
+if [[ ! -e "$LOG_FILE" ]]; then
+    echo "$0: could not create log file: $LOG_FILE"
+    exit 1
+fi
+if [[ ! -w "$LOG_FILE" ]]; then
+    echo "$0: log file not writable: $LOG_FILE"
+    exit 1
+fi
+
+if [[ $V_FLAG -gt 1 ]]; then
+    echo "Will write contents to $LOG_FILE" 1>&2
+fi
+
+# XXX - FIXME
+#
+# Currently the exit codes will not be correctly tested. The idea is here but
+# the exit codes will not be tested right so if for instance make test fails it
+# will still say that all tests passed. This will have to be fixed to make the
+# testing of the exit codes useful but this will come at a later date.
+#
+# Yes it's very amusing that a script called bug_report.sh has a bug but amusing
+# as it would have been I cannot say this was actually intentional! :-) I had
+# thought of it originally but I neglected to address it until after a commit.
+# This bug will be fixed at a later date as I must go do other things now. Still
+# better to have it after all as who can't use more laughs? :-)
+#
+# XXX - FIXME
+echo "## TIME OF REPORT: $(date)" | tee -a -- "$LOG_FILE"
+echo "## RUNNING make clobber: " | tee -a -- "$LOG_FILE"
+make clobber | tee -a -- "$LOG_FILE"  1>&2
+status=$?
+if [[ "$status" -ne 0 ]]; then
+    EXIT_CODE=10
+    echo "make clobber failed with exit code $status: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
+fi
+echo '' >> "$LOG_FILE" 1>&2
+echo "## RUNNING make all: " | tee -a -- "$LOG_FILE" 1>&2
+make all | tee -a -- "$LOG_FILE"  1>&2
+status=$?
+if [[ "$status" -ne 0 ]]; then
+    EXIT_CODE=11
+    echo "make all failed with exit code $status: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
+fi
+echo '' >> "$LOG_FILE" 1>&2
+echo "## RUNNING make test: " | tee -a -- "$LOG_FILE" 1>&2
+make test | tee -a -- "$LOG_FILE"  1>&2
+status=$?
+if [[ "$status" -ne 0 ]]; then
+    EXIT_CODE=12
+    echo "make test failed with exit code $status: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
+fi
+
+echo '' >> "$LOG_FILE" 1>&2
+echo "## RUNNING hostchk.sh -v 3: " | tee -a -- "$LOG_FILE" 1>&2
+./hostchk.sh -v 3 | tee -a -- "$LOG_FILE"  1>&2
+status=$?
+if [[ "$status" -ne 0 ]]; then
+    EXIT_CODE=13
+    echo "hostchk.sh failed with exit code $status: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
+fi
+
+
+if [[ "$EXIT_CODE" -ne 0 ]]; then
+    echo "Found one more issues. Please file an issue on the GitHub issues page and attach" 1>&2
+    echo "the file $LOG_FILE in your report" 1>&2
+else
+    echo "All tests PASSED" 1>&2
+    echo "A log of the above tests was saved to $LOG_FILE" 1>&2
+    echo "If you feel everything is in order you may safely delete the file." 1>&2
+fi
+
+if [[ "$V_FLAG" -gt 1 ]]; then
+    echo "About to exit with exit code: $EXIT_CODE" 1>&2
+fi
+exit "$EXIT_CODE"
