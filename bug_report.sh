324149a5cddf353b7744ae7e0f226dd0637b192b
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2022-10-15 08:21:53 -0700

    New version of bug_report.sh - many updates
    This version has many additions and improvements. Since there are
    probably fewer improvements I'll document those first.
    
    Instead of trying to keep track of the exit code each time a test fails
    we start the exit code at 10. Then if a test fails it will be
    incremented without any concern of what the previous value was. This is
    because the only reason the exit code matters is to indicate that a
    problem was detected. The error message is what tells us what really
    went wrong and this way we can rearrange tests, add new tests in the
    middle etc. (as some tests might be thought of later on but which should
    be done before other tests) without having to then update all the other
    references. At the end if the exit code is not 10 (which was arbitrarily
    selected) we know that at least one issue was detected. If it's 10 at
    the end we set it back to 0 so that the script will not fail when all is
    actually okay.  This greatly simplifies adding new tests.
    
    Add function is_exec which tests if a file exists, is a regular file and
    is executable. If any of those conditions fail it reports it as an error
    and returns 1. This function is used later in the script that acts on
    the new TOOLS variable (at the top in case we have to update the list of
    tools).
    
    Check that all our tools are available and executable (or at least those
    that were referred to in the GitHub comment - it's possible some are
    missing from the list but it's easy to update by adding to the TOOLS
    variable). If the tool is not executable it is an error that is
    reported. Otherwise run -h on the tool. We might only need the version
    eventually but this is not decided and not all tools (scripts in
    particular) have versions yet.
    
    The script now runs the following commands:
    
        uname -a
        which cc
        cc -v
        which make
    I forgot to check the make version which due to the improvements with
    exit codes I can easily add later.
    
        which tar
    
    We don't need to check that tar has the correct options implemented
    because hostchk.sh will do that for us.
    
    These are only what was added in this commit. Much more needs to be done
    and it could certainly be made even more modular but this is great
    progress. The next step is probably to add simple C files that should be
    test compiled as well as running some of the scripts like run_bison.sh
    and run_flex.sh. Some of this might come later today or it might be
    another day.

--- bug_report.sh
+++ bug_report.sh
@@ -14,7 +14,16 @@
 # NOTE: This is a work in progress.
 #
 
-export BUG_REPORT_VERSION="0.1 2022-10-14"
+# All of our tools. Kept towards top of file in case the list needs to be
+# changed.
+export TOOLS="./run_bison.sh ./run_flex.sh ./hostchk.sh ./dbg ./dyn_test ./fnamchk
+	      ./ioccc_test.sh ./iocccsize ./iocccsize_test.sh ./chkentry ./jnum_gen
+	      ./jnum_chk ./jparse ./jparse_test.sh ./jstr_test.sh ./jstrencode
+	      ./jstrdecode ./mkiocccentry ./mkiocccentry_test.sh ./prep.sh
+	      ./reset_tstamp.sh ./txzchk ./txzchk_test.sh ./utf8_test ./verge
+	      ./vermod.sh"
+
+export BUG_REPORT_VERSION="0.2 2022-10-15"
 export FAILURE_SUMMARY=
 export DBG_LEVEL="0"
 export V_FLAG="0"
@@ -62,8 +71,10 @@ while getopts :hVv:D: flag; do
 done
 
 
-# assume all tests will pass
-EXIT_CODE=0
+# Assume all tests will pass. We start at 10 but whenever any test fails it is
+# incremented. This makes it easy to rearrange the tests. If at the end the
+# EXIT_CODE is still 10 we know every test passed.
+EXIT_CODE=10
 
 # NOTE: log file does not have an underscore in the name because we want to
 # distinguish it from this script which does have an underscore in it.
@@ -82,11 +93,86 @@ if [[ ! -w "$LOG_FILE" ]]; then
     exit 1
 fi
 
+
+# is_exec   - determine if arg exists, is a regular file and is executable
+function is_exec()
+{
+    if [[ $# -ne 1 ]]; then
+	echo "$0: ERROR: expected 1 arg to is_exec, found $#" | tee -a -- "$LOG_FILE"
+	return 1
+    else
+	typeset f="$1"
+	if [[ ! -e "$f" ]]; then
+	    echo "$0: ERROR: $1 does not exist" | tee -a -- "$LOG_FILE"
+	    return 1
+	fi
+	if [[ ! -f "$f" ]]; then
+	    echo "$0: ERROR: $1 is not a regular file" | tee -a -- "$LOG_FILE"
+	    return 1
+	fi
+	if [[ ! -x "$f" ]]; then
+	    echo "$0: ERROR: $1 is not executable" | tee -a -- "$LOG_FILE"
+	    return 1
+	fi
+	return 0
+    fi
+}
+
 if [[ $V_FLAG -gt 1 ]]; then
     echo "Will write contents to $LOG_FILE" 1>&2
 fi
 
 echo "## TIME OF REPORT: $(date)" | tee -a -- "$LOG_FILE"
+
+# uname -a: get system information
+echo "## RUNNING uname -a: " | tee -a -- "$LOG_FILE"
+uname -a | tee -a -- "$LOG_FILE"
+status=${PIPESTATUS[0]}
+if [[ "$status" -ne 0 ]]; then
+    EXIT_CODE=10
+    echo "$0: ERROR: uname -a failed with exit code $status: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
+    FAILURE_SUMMARY="$FAILURE_SUMMARY
+    uname -a non-zero exit code: $status"
+fi
+echo | tee -a -- "$LOG_FILE"
+
+# which cc: get all paths for cc
+echo "## RUNNING which -a cc: " | tee -a -- "$LOG_FILE"
+which cc | tee -a -- "$LOG_FILE"
+status=${PIPESTATUS[0]}
+if [[ "$status" -ne 0 ]]; then
+    ((EXIT_CODE++))
+    echo "$0: ERROR: which cc failed with exit code $status: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
+    FAILURE_SUMMARY="$FAILURE_SUMMARY
+    which cc non-zero exit code: $status"
+fi
+echo | tee -a -- "$LOG_FILE"
+
+# cc -v: get compiler version
+echo "## RUNNING cc -v: " | tee -a -- "$LOG_FILE"
+cc -v 2>&1 | tee -a -- "$LOG_FILE"
+status=${PIPESTATUS[0]}
+if [[ "$status" -ne 0 ]]; then
+    ((EXIT_CODE++))
+    echo "$0: ERROR: cc -v failed with exit code $status: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
+    FAILURE_SUMMARY="$FAILURE_SUMMARY
+    cc -v non-zero exit code: $status"
+fi
+echo | tee -a -- "$LOG_FILE"
+
+# which make: get path to make tool
+echo "## RUNNING which make: " | tee -a -- "$LOG_FILE"
+which -a make | tee -a -- "$LOG_FILE"
+status=${PIPESTATUS[0]}
+if [[ "$status" -ne 0 ]]; then
+    ((EXIT_CODE++))
+    echo "$0: ERROR: which -a make failed with exit code $status: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
+    FAILURE_SUMMARY="$FAILURE_SUMMARY
+    which -a make non-zero exit code: $status"
+fi
+echo | tee -a -- "$LOG_FILE"
+
+# make clobber: start clean
 echo "## RUNNING make clobber: " | tee -a -- "$LOG_FILE"
 make clobber | tee -a -- "$LOG_FILE"  1>&2
 # The below references to PIPESTATUS (instead of $?) fix the bug introduced in commit
@@ -102,12 +188,14 @@ make clobber | tee -a -- "$LOG_FILE"  1>&2
 # I caused. You're welcome! :-)
 status=${PIPESTATUS[0]}
 if [[ "$status" -ne 0 ]]; then
-    EXIT_CODE=10
+    ((EXIT_CODE++))
     echo "$0: ERROR: make clobber failed with exit code $status: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
     FAILURE_SUMMARY="$FAILURE_SUMMARY
     make clobber non-zero exit code: $status"
 fi
-echo '' >> "$LOG_FILE" 1>&2
+echo '' | tee -a -- "$LOG_FILE"
+
+# make all: compile everything before we do anything else
 echo "## RUNNING make all: " | tee -a -- "$LOG_FILE" 1>&2
 # NOTE: This will indirectly call make fast_hostchk which, if it reports an
 # issue, will be reported here. However we also directly invoke hostchk.sh later
@@ -130,38 +218,74 @@ echo "## RUNNING make all: " | tee -a -- "$LOG_FILE" 1>&2
 # really is likely that, if the script fails, you will be unable to successfully
 # use the mkiocccentry repo to submit a correct IOCCC entry.
 #
-make all | tee -a -- "$LOG_FILE"  1>&2
+make all | tee -a -- "$LOG_FILE"
 status=${PIPESTATUS[0]}
 if [[ "$status" -ne 0 ]]; then
-    EXIT_CODE=11
+    ((EXIT_CODE++))
     echo "$0: ERROR: make all failed with exit code $status: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
     FAILURE_SUMMARY="$FAILURE_SUMMARY
     make all non-zero exit code: $status"
 fi
-echo '' >> "$LOG_FILE" 1>&2
-echo "## RUNNING make test: " | tee -a -- "$LOG_FILE" 1>&2
+echo | tee -a -- "$LOG_FILE"
+
+# make test: run the IOCCC toolkit test suite
+echo "## RUNNING make test: " | tee -a -- "$LOG_FILE"
 make test | tee -a -- "$LOG_FILE"  1>&2
 status=${PIPESTATUS[0]}
 if [[ "$status" -ne 0 ]]; then
-    EXIT_CODE=12
+    ((EXIT_CODE++))
     echo "$0: ERROR: make test failed with exit code $status: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
     FAILURE_SUMMARY="$FAILURE_SUMMARY
     make test non-zero exit code: $status"
 fi
+echo '' >> "$LOG_FILE" 1>&2
+
+# which tar: find the path to tar
+echo "## RUNNING which tar: " | tee -a -- "$LOG_FILE"
+which tar | tee -a -- "$LOG_FILE"
+status=${PIPESTATUS[0]}
+if [[ "$status" -ne 0 ]]; then
+    ((EXIT_CODE++))
+    echo "$0: ERROR: which tar failed with exit code $status: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
+    FAILURE_SUMMARY="$FAILURE_SUMMARY
+    which tar non-zero exit code: $status"
+fi
+echo '' | tee -a -- "$LOG_FILE"
+# NOTE: we don't need to check if tar accepts the correct options in this script
+# because hostchk.sh will do that later on.
+for f in $TOOLS; do
+    if is_exec "$f"; then
+	echo "## Checking if $f is executable" | tee -a -- "$LOG_FILE"
+	echo "$0: $f is executable" | tee -a -- "$LOG_FILE"
+	echo | tee -a -- "$LOG_FILE"
+	echo "## RUNNING $f -h" | tee -a -- "$LOG_FILE"
+	"$f" -h | tee -a -- "$LOG_FILE"
+	echo | tee -a -- "$LOG_FILE"
+    else
+	((EXIT_CODE++))
+	echo "$0: ERROR: $f is not executable: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
+	FAILURE_SUMMARY="$FAILURE_SUMMARY
+	$f cannot be executed"
+    fi
+done
+
 
 echo '' >> "$LOG_FILE" 1>&2
 echo "## RUNNING hostchk.sh -v 3: " | tee -a -- "$LOG_FILE" 1>&2
 ./hostchk.sh -v 3 | tee -a -- "$LOG_FILE"  1>&2
 status=${PIPESTATUS[0]}
 if [[ "$status" -ne 0 ]]; then
-    EXIT_CODE=13
+    ((EXIT_CODE++))
     echo "$0: ERROR: hostchk.sh failed with exit code $status: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
     FAILURE_SUMMARY="$FAILURE_SUMMARY
     hostchk.sh test non-zero exit code: $status"
 fi
 
 
-if [[ "$EXIT_CODE" -ne 0 ]]; then
+# The reason we check for 10 is that's the default value and each time a test
+# fails the exit code is incremented. This makes it easy to rearrange the tests
+# without having to worry to update the exit codes.
+if [[ "$EXIT_CODE" -ne 10 ]]; then
     echo 1>&2
     echo "One or more problems occurred:" | tee -a -- "$LOG_FILE"
     echo "$FAILURE_SUMMARY" | tee -a -- "$LOG_FILE"
@@ -175,6 +299,9 @@ if [[ "$EXIT_CODE" -ne 0 ]]; then
     echo "making sure to attach $LOG_FILE with your report. You may" | tee -a -- "$LOG_FILE"
     echo "instead email the Judges." | tee -a -- "$LOG_FILE"
 else
+    # here we do set the exit code back to 0 if all is okay so the exit status
+    # of the script does not trigger a false failure.
+    EXIT_CODE=0
     echo "All tests PASSED" | tee -a -- "$LOG_FILE"
     echo | tee -a -- "$LOG_FILE"
     echo "A log of the above tests was saved to $LOG_FILE." | tee -a -- "$LOG_FILE"
