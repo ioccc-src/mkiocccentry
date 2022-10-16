6a62665e77de2cf0b338b0feca482e8411d6d371
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2022-10-16 03:06:47 -0700

    Release bug_report.sh version 0.3 - make modular
    In order to make the script much easier to maintain it is now much more
    modular. Instead of having to repeat basically the same code over and
    over again it is in a single loop (or two if you count the check for
    execute bit and running -h on each tool). The loop also controls the
    exit code as well as the command number (see below) so that it's easy to
    add a new command in the list even if one of the commands has to be
    somewhere in the list (one would have to update the base codes i.e. the
    index but that wouldn't take much time). To add a command all one has to
    do is add an element to the new COMMANDS array (making sure to double
    quote it) and add a comment as to what it is and its purpose.
    
    This might or might not be called experimental but I think it's a good
    change with perhaps one downside (not sure it is a downside though): you
    can't go to a specific line in the script and determine what command
    will run.  However you can easily identify which command it is in the
    array simply by the index and one could easily update the loop so it
    says what command number it is.
    
    Also the script will say what is about to be run and this seems like
    a vast improvement as it makes the script much easier to maintain and as
    a bonus it's more compact as well. If however there is reason to not
    have it this way I have thought of another way to help with the exit
    code issue as well. It would not be as modular but it would at least
    make it a bit easier to identify the command to run (in fact, as noted
    above, this commit adds what amounts to an identification number for
    each command which I think is also a bonus).
    
    Finally the script now calls ./run_bison.sh -v 1 and ./run_flex.sh -v 1.
    
    I believe that makes comment
    https://github.com/ioccc-src/mkiocccentry/issues/248#issuecomment-1153283608
    almost entirely complete. The only things left to be done (besides the
    things not explicitly stated because they weren't thought of yet) is
    that not all scripts have versions in the help/usage string. These can
    be added later.

--- bug_report.sh
+++ bug_report.sh
@@ -14,6 +14,8 @@
 # NOTE: This is a work in progress.
 #
 
+BUG_REPORT_VERSION="0.3 2022-10-16"
+
 # All of our tools. Kept towards top of file in case the list needs to be
 # changed.
 export TOOLS="./run_bison.sh ./run_flex.sh ./hostchk.sh ./dbg ./dyn_test ./fnamchk
@@ -23,7 +25,6 @@ export TOOLS="./run_bison.sh ./run_flex.sh ./hostchk.sh ./dbg ./dyn_test ./fnamc
 	      ./reset_tstamp.sh ./txzchk ./txzchk_test.sh ./utf8_test ./verge
 	      ./vermod.sh ./jsemcgen.sh ./jsemtblgen"
 
-export BUG_REPORT_VERSION="0.2 2022-10-15"
 export FAILURE_SUMMARY=
 export DBG_LEVEL="0"
 export V_FLAG="0"
@@ -121,100 +122,34 @@ fi
 
 echo "## TIME OF REPORT: $(date)" | tee -a -- "$LOG_FILE"
 
-# uname -a: get system information
-echo "## RUNNING uname -a: " | tee -a -- "$LOG_FILE"
-uname -a | tee -a -- "$LOG_FILE"
-status=${PIPESTATUS[0]}
-if [[ "$status" -ne 0 ]]; then
-    EXIT_CODE=10
-    echo "$0: ERROR: uname -a failed with exit code $status: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
-    FAILURE_SUMMARY="$FAILURE_SUMMARY
-    uname -a non-zero exit code: $status"
-fi
-echo | tee -a -- "$LOG_FILE"
-
-# which cc: get all paths for cc
-echo "## RUNNING which -a cc: " | tee -a -- "$LOG_FILE"
-which cc | tee -a -- "$LOG_FILE"
-status=${PIPESTATUS[0]}
-if [[ "$status" -ne 0 ]]; then
-    EXIT_CODE=11
-    echo "$0: ERROR: which cc failed with exit code $status: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
-    FAILURE_SUMMARY="$FAILURE_SUMMARY
-    which cc non-zero exit code: $status"
-fi
-echo | tee -a -- "$LOG_FILE"
-
-# cc -v: get compiler version
-echo "## RUNNING cc -v: " | tee -a -- "$LOG_FILE"
-cc -v 2>&1 | tee -a -- "$LOG_FILE"
-status=${PIPESTATUS[0]}
-if [[ "$status" -ne 0 ]]; then
-    EXIT_CODE=12
-    echo "$0: ERROR: cc -v failed with exit code $status: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
-    FAILURE_SUMMARY="$FAILURE_SUMMARY
-    cc -v non-zero exit code: $status"
-fi
-echo | tee -a -- "$LOG_FILE"
-
-# which make: get path to make tool
-echo "## RUNNING which make: " | tee -a -- "$LOG_FILE"
-which -a make | tee -a -- "$LOG_FILE"
-status=${PIPESTATUS[0]}
-if [[ "$status" -ne 0 ]]; then
-    EXIT_CODE=13
-    echo "$0: ERROR: which -a make failed with exit code $status: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
-    FAILURE_SUMMARY="$FAILURE_SUMMARY
-    which -a make non-zero exit code: $status"
-fi
-echo | tee -a -- "$LOG_FILE"
-
-# make -v: get make version
-echo "## RUNNING make -v: " | tee -a -- "$LOG_FILE"
-make -v | tee -a -- "$LOG_FILE"
-status=${PIPESTATUS[0]}
-if [[ "$status" -ne 0 ]]; then
-    EXIT_CODE=14
-    echo "$0: ERROR: make -v failed with exit code $status: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
-    FAILURE_SUMMARY="$FAILURE_SUMMARY
-    make -v non-zero exit code: $status"
-fi
-echo | tee -a -- "$LOG_FILE"
-
-
-
-# make clobber: start clean
-echo "## RUNNING make clobber: " | tee -a -- "$LOG_FILE"
-make clobber | tee -a -- "$LOG_FILE"  1>&2
-# The below references to PIPESTATUS (instead of $?) fix the bug introduced in commit
-# 8343c4b8cb97e52df64fe8973e68f0d83c6090e1 where the exit status of each command
-# was not checked properly which meant that even if a test failed it would not
-# be reported as an issue which rather defeated the purpose of this script.
+# Make array of all commands we need.
 #
-# As amusing as the thought is that there's a bug in a script to help report
-# bugs and issues, this bug was not in fact intentional. :-) I had thought of it
-# earlier on but I neglected to address it before the commit as I moved on with
-# adding the tests. However since the world definitely needs more reasons to
-# laugh I'm keeping this comment here for the sake of humour and the irony that
-# I caused. You're welcome! :-)
-status=${PIPESTATUS[0]}
-if [[ "$status" -ne 0 ]]; then
-    EXIT_CODE=15
-    echo "$0: ERROR: make clobber failed with exit code $status: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
-    FAILURE_SUMMARY="$FAILURE_SUMMARY
-    make clobber non-zero exit code: $status"
-fi
-echo '' | tee -a -- "$LOG_FILE"
-
-# make all: compile everything before we do anything else
-echo "## RUNNING make all: " | tee -a -- "$LOG_FILE" 1>&2
-# NOTE: This will indirectly call make fast_hostchk which, if it reports an
+# This is done this way so that we can use each command's index as an offset for
+# an exit code that will be added to 11 (the first exit code that is an error
+# beyond the one that does not have an individual code, see below).
+#
+# Since arrays also don't have to be assigned contiguously this will allow us to
+# also spread commands out for exit codes. We shouldn't need more than the
+# amount of exit codes available so this should be okay.
+#
+# There is an exception to the exit codes here and that's the check for if each
+# of our tools is executable. For that we use a single exit code 10. This is not
+# done until later on but with exit code 10 so that it's out of the way of the
+#
+# We assign the commands to the array one at a time so we have control of the
+# index for the command which will also be part of the error code if it goes
+# wrong.
+#
+# The 0th element will be exit code 11 if it fails; the 1st element will be 11
+# and so on.
+#
+# NOTE: make all will indirectly call make fast_hostchk which, if it reports an
 # issue, will be reported here. However we also directly invoke hostchk.sh later
 # which will also report an issue so that one would likely see the warning more
 # than once. If you only see the warning once then there probably is also a
-# problem. The warning issued will suggest that you can file the issue with
-# this script and it also suggests that you can get more information with
-# running hostchk.sh directly.
+# problem. The warning issued will suggest that you can file the issue with this
+# script and it also suggests that you can get more information with running
+# hostchk.sh directly.
 #
 # Obviously one needn't run this script a second or third time just because it
 # runs hostchk.sh which suggests that you run this script each time it exits
@@ -229,41 +164,46 @@ echo "## RUNNING make all: " | tee -a -- "$LOG_FILE" 1>&2
 # really is likely that, if the script fails, you will be unable to successfully
 # use the mkiocccentry repo to submit a correct IOCCC entry.
 #
-make all | tee -a -- "$LOG_FILE"
-status=${PIPESTATUS[0]}
-if [[ "$status" -ne 0 ]]; then
-    EXIT_CODE=16
-    echo "$0: ERROR: make all failed with exit code $status: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
-    FAILURE_SUMMARY="$FAILURE_SUMMARY
-    make all non-zero exit code: $status"
-fi
-echo | tee -a -- "$LOG_FILE"
+BASE_ERR_EXIT_CODE=11
+COMMANDS[0]="uname -a"			# uname -a		get system information
+COMMANDS[1]="which cc"			# which cc		get path for cc
+COMMANDS[2]="cc -v"			# cc -v			get compiler version
+COMMANDS[3]="which make"		# which make		get make path
+COMMANDS[4]="make -v"			# make -v		get make version
+COMMANDS[5]="make clobber"		# make clobber		start clean
+COMMANDS[6]="make all"			# make all		compile everything
+COMMANDS[7]="make test"			# make test		run mkiocccentry test suite
+COMMANDS[8]="which tar"			# which tar		get path to tar
+COMMANDS[9]="./hostchk.sh -v 3"		# ./hostchk.sh -v 3	various checks on the host
+COMMANDS[10]="./run_bison.sh -v 1"	# ./run_bison.sh -v 1	get details about bison
+COMMANDS[11]="./run_flex.sh -v 1"	# ./run_flex.sh -v 1	get details about flex
 
-# make test: run the IOCCC toolkit test suite
-echo "## RUNNING make test: " | tee -a -- "$LOG_FILE"
-make test | tee -a -- "$LOG_FILE"  1>&2
-status=${PIPESTATUS[0]}
-if [[ "$status" -ne 0 ]]; then
-    EXIT_CODE=17
-    echo "$0: ERROR: make test failed with exit code $status: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
-    FAILURE_SUMMARY="$FAILURE_SUMMARY
-    make test non-zero exit code: $status"
-fi
-echo '' >> "$LOG_FILE" 1>&2
+# NOTE: The below references to PIPESTATUS (instead of $?) fix the bug
+# introduced in commit 8343c4b8cb97e52df64fe8973e68f0d83c6090e1 where the exit
+# status of each command was not checked properly which meant that even if a
+# test failed it would not be reported as an issue which rather defeated the
+# purpose of this script.
+#
+# As amusing as the thought is that there's a bug in a script to help report
+# bugs and issues, this bug was not in fact intentional. :-) I had thought of it
+# earlier on but I neglected to address it before the commit as I moved on with
+# adding the tests. However since the world definitely needs more reasons to
+# laugh I'm keeping this comment here for the sake of humour and the irony that
+# I caused. You're welcome! :-)
 
-# which tar: find the path to tar
-echo "## RUNNING which tar: " | tee -a -- "$LOG_FILE"
-which tar | tee -a -- "$LOG_FILE"
-status=${PIPESTATUS[0]}
-if [[ "$status" -ne 0 ]]; then
-    EXIT_CODE=18
-    echo "$0: ERROR: which tar failed with exit code $status: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
-    FAILURE_SUMMARY="$FAILURE_SUMMARY
-    which tar non-zero exit code: $status"
-fi
-echo '' | tee -a -- "$LOG_FILE"
-# NOTE: we don't need to check if tar accepts the correct options in this script
-# because hostchk.sh will do that later on.
+for i in "${!COMMANDS[@]}"; do
+    echo "## RUNNING ${COMMANDS[$i]}: " | tee -a -- "$LOG_FILE"
+    command ${COMMANDS[$i]} | tee -a -- "$LOG_FILE"
+    status=${PIPESTATUS[0]}
+    if [[ "$status" -ne 0 ]]; then
+	EXIT_CODE=$((i + BASE_ERR_EXIT_CODE))
+	echo "$0: ERROR: ${COMMANDS[$i]} failed with exit code $status: new exit code $EXIT_CODE" | tee -a -- "$LOG_FILE"
+	FAILURE_SUMMARY="$FAILURE_SUMMARY
+	${COMMANDS[$i]} non-zero exit code: $status"
+    fi
+    echo '' | tee -a -- "$LOG_FILE"
+done
+
 for f in $TOOLS; do
     if is_exec "$f"; then
 	echo "## Checking if $f is executable" | tee -a -- "$LOG_FILE"
@@ -273,25 +213,12 @@ for f in $TOOLS; do
 	"$f" -h 2>&1 | tee -a -- "$LOG_FILE"
 	echo | tee -a -- "$LOG_FILE"
     else
-	EXIT_CODE=19
+	EXIT_CODE=10
 	echo "$0: ERROR: $f is not executable: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
 	FAILURE_SUMMARY="$FAILURE_SUMMARY
 	$f cannot be executed"
     fi
 done
-
-
-echo '' >> "$LOG_FILE" 1>&2
-echo "## RUNNING hostchk.sh -v 3: " | tee -a -- "$LOG_FILE" 1>&2
-./hostchk.sh -v 3 | tee -a -- "$LOG_FILE"  1>&2
-status=${PIPESTATUS[0]}
-if [[ "$status" -ne 0 ]]; then
-    EXIT_CODE=20
-    echo "$0: ERROR: hostchk.sh failed with exit code $status: new exit code: $EXIT_CODE" | tee -a -- "$LOG_FILE"
-    FAILURE_SUMMARY="$FAILURE_SUMMARY
-    hostchk.sh test non-zero exit code: $status"
-fi
-
 
 if [[ "$EXIT_CODE" -ne 0 ]]; then
     echo 1>&2
