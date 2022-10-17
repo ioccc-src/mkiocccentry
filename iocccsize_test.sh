827ded902077ff2e98d890c84a2953fd7565af08
Landon Curt Noll <github-mail@asthe.com>
2022-10-16 19:44:36 -0700

    Improved iocccsize_test.sh script & iocccsize_test(8) page.
    Fixed description of debugging.
    
    Improved the tests by checking both iocccsize -v 0 and iocccsize
    -v 1 output.  Verify both wc -c and expected Rule 2a values.
    
    Added -v 3 debugging level to report on what was expected and
    discovered during each test.
    
    Added an explicit version string for iocccsize_test.sh.
    
    Combined exit codes for both -h and -V.
    
    Changed -t tool to -i iocccsize.
    
    Added -w work_dir command line option.
    
    Pre-remove work_dir and then make work_dir during the test.
    
    Added -l limit_ioccc.sh to change the location of limit_ioccc.sh.
    Added -l . to disable use of limit_ioccc.sh.
    
    Added a number of internal tests and sanity checks to the
    overall test suite.
    
    Fixed multiple style, portability, and robustness issues.
    Added lots of comments.
    
    Added an EXAMPLE section to iocccsize_test(8).
    
    Updated iocccsize_test(8) man page accordingly.

--- iocccsize_test.sh
+++ iocccsize_test.sh
@@ -8,126 +8,256 @@
 #
 # IOCCC mods in 2019,2021,2022 by chongo (Landon Curt Noll) ^oo^
 
-export PATH='/bin:/usr/bin:/usr/local/bin:/usr/pkg/bin'
-export ENV=''
-export CDPATH=''
-export LANG=C
-export DIGRAPHS='true'	# assume #define DIGRAPHS
-export TRIGRAPHS='true'	# assume #define TRIGRAPHS
+# setup
+#
+export DIGRAPHS=	# assume #undef DIGRAPHS
+export TRIGRAPHS=	# assume #undef TRIGRAPHS
+export LIMIT_IOCCC="./limit_ioccc.sh"
+export WORK_DIR="./test_iocccsize"
+export IOCCCSIZE="./iocccsize"
 export EXIT_CODE=0
+export IOCCCSIZE_ARGS="-v 1 --"
+export V_FLAG=0
 
-export __tool="./iocccsize"
-export __tool_args="--"
-export __verbose=
-export __build=
+export TEST_IOCCCSIZE_VERSION="1.1 2022-10-16"
 
-usage()
-{
-	echo "usage: $0 [-h] [-b] [-v lvl] [-V] [-t tool]"
-	echo
-	echo "	-h	    print usage message and exit 2"
-	echo "	-b	    make distclean all (def: make all)"
-	echo "	-v lvl	    set debugging level to lvl (def: no debugging)"
-	echo "	-V	    print tool version and exit 3"
-	echo "	-t tool	    test with tool (def: test with ./iocccsize)"
-	echo
-	echo "Exit codes:"
-	echo
-	echo "	0   all is OK"
-	echo "	1   one or more test failed"
-	echo "	2   usage message printed"
-	echo "	3   tool version printed"
-}
+export USAGE="usage: $0 [-h] [-v lvl] [-V] [-i iocccsize] [-w work_dir] [-l limit]
 
-while getopts 'hbv:Vt:' opt; do
-	case "$opt" in
-	(h)
-		usage 1>&2
-		exit 2
-		;;
-	(b)
-		__build="distclean"
-		;;
-	(v)
-		__verbose=true
-		__tool_args="-v ${OPTARG} --"
-		;;
-	(V)
-		"$__tool" -V
-		exit 3
-		;;
-	(t)
-		__tool="$OPTARG"
-		;;
-	(*)
-		echo "unknown option" 1>&2
-		echo 1>&2
-		usage 1>&2
-		exit 2
-	esac
+    -h		    print usage message and exit 2
+    -v lvl	    set debugging level to lvl (def: 0 ==> no debugging)
+    -V		    print tool version and exit 2
+    -i iocccisze    path to iocccsize tool (def: test with ./iocccsize)
+    -w work_dir	    working directory that is removed & rebuilt during the test (def: ./test_iocccsize)
+    -l limit	    path to limit_ioccc.sh executable shell script (def: ./limit_ioccc.sh)
+		    A limit of . (dot) will disable use of a executable shell script.
+
+Exit codes:
+     0   all is OK
+     1   one or more tests failed
+     2   help or version mode used
+ >= 10	 internal error
+
+$0 version: $TEST_IOCCCSIZE_VERSION"
+
+# parse args
+#
+while getopts 'hv:Vi:w:l:' opt; do
+    case "$opt" in
+    h)	echo "$USAGE" 1>&2
+	exit 2
+	;;
+    v)	V_FLAG="$OPTARG"
+	;;
+    V)	echo "$TEST_IOCCCSIZE_VERSION"
+	exit 2
+	;;
+    i)	IOCCCSIZE="$OPTARG"
+	;;
+    w)	WORK_DIR="$OPTARG"
+	;;
+    l)	LIMIT_IOCCC="$OPTARG"
+	;;
+    *)	echo "$0: ERROR: unknown option: -$opt" 1>&2
+	echo 1>&2
+	echo "$USAGE" 1>&2
+	exit 2
+	;;
+    esac
 done
 shift $((OPTIND - 1))
 
-eval make "${__build}" all 2>&1 | grep -v 'Nothing to be done for'
-
-if [[ ! -d test_iocccsize ]]; then
-	mkdir -p test_iocccsize
-fi
-
-# Change DIGRAPHS and TRIGRAPHS according to limit_ioccc.h
+# validate ./iocccsize
 #
-if [[ -f limit_ioccc.sh ]]; then
-	source limit_ioccc.sh
+if [[ ! -e $IOCCCSIZE ]]; then
+    echo "$0: ERROR: iocccsize does not exist: $IOCCCSIZE" 1>&2
+    exit 10
+fi
+if [[ ! -f $IOCCCSIZE ]]; then
+    echo "$0: ERROR: iocccsize is not a file: $IOCCCSIZE" 1>&2
+    exit 11
+fi
+if [[ ! -x $IOCCCSIZE ]]; then
+    echo "$0: ERROR: limit_ioccc.h is not a executable file: $IOCCCSIZE" 1>&2
+    exit 12
 fi
 
+# validate readable limit_ioccc.h
+#
+if [[ $LIMIT_IOCCC != "." ]]; then
+    if [[ ! -e $LIMIT_IOCCC ]]; then
+	echo "$0: ERROR: limit_ioccc.h does not exist: $LIMIT_IOCCC" 1>&2
+	exit 13
+    fi
+    if [[ ! -f $LIMIT_IOCCC ]]; then
+	echo "$0: ERROR: limit_ioccc.h is not a file: $LIMIT_IOCCC" 1>&2
+	exit 14
+    fi
+    if [[ ! -r $LIMIT_IOCCC ]]; then
+	echo "$0: ERROR: limit_ioccc.h is not a readable file: $LIMIT_IOCCC" 1>&2
+	exit 15
+    fi
+fi
+
+# create an empty working directory
+#
+rm -rf  "$WORK_DIR"
+status="$?"
+if [[ $status -ne 0 ]]; then
+    echo "$0: ERROR: rm -rf $WORK_DIR failed, error code: $status" 1>&2
+    exit 16
+fi
+mkdir -p "$WORK_DIR"
+status="$?"
+if [[ $status -ne 0 ]]; then
+    echo "$0: ERROR: mkdir -p $WORK_DIR failed, error code: $status" 1>&2
+    exit 17
+fi
+
+# Change DIGRAPHS and TRIGRAPHS according to limit_ioccc.h, unless .
+#
+if [[ $LIMIT_IOCCC != "." ]]; then
+    # warning: ShellCheck can't follow non-constant source. Use a directive to specify location.
+    # shellcheck disable=SC1090
+    source "$LIMIT_IOCCC"
+    status="$?"
+    if [[ $status -ne 0 ]]; then
+	echo "$0: ERROR: source $LIMIT_IOCCC error code: $status" 1>&2
+	exit 18
+    fi
+fi
+
+# obtain a wc value of a file
+#
 get_wc()
 {
-	declare file="$1"
-	declare field="$2"
-	wc "$file" | sed -e's/^ *//; s/  */ /g' | cut -d' ' "-f$field"
+    declare file="$1"
+    declare field="$2"
+
+    wc "$file" | sed -e's/^ *//; s/  */ /g' | cut -d' ' "-f$field"
 }
 
+# perform a specific iocccsize test
+#
+# usage:
+#	test_size c_src_file "expected_Rule_2b expected_Rule_2a expected_keywords"
+#
+# NOTE: The expected_keywords (3rd expected arg) is ignored.
+#
 test_size()
 {
-	declare file="test_iocccsize/$1"
-	declare expect="$2"
-	declare gross_count
-	declare got
+    declare file="$WORK_DIR/$1"	# C program file to test
+    declare expect="$2"		# expected args of test
+    #
+    declare expect_2a		# expected Rule 2b value
+    declare expect_2b		# expected Rule 2b value
+    #
+    declare iocccsize_v0	# iocccsize -v 0 output
+    declare iocccsize_v1	# iocccsize -v 1 output
+    declare iocccsize_v0_2b	# iocccsize -v 0 Rule 2b (1st arg)
+    declare iocccsize_v1_2a	# iocccsize -v 1 Rule 2a (2nd arg)
+    declare iocccsize_v1_2b	# iocccsize -v 1 Rule 2b (1st arg)
+    #
+    declare wc_bytes		# wc -c bytes
+    #
+    declare pass		# test pass true or false
 
-	got=$("$__tool" "$__tool_args" "$file" 2>/dev/null)
-	if [[ -n $__verbose ]]; then
-		gross_count=$(echo "$got" | cut -d' ' -f2)
-		bytes=$(get_wc "$file" 3)
-		if [[ $gross_count != "$bytes" ]]; then
-			echo "FAIL $file: got $gross_count != wc $bytes"
-			EXIT_CODE=1
-			return
-		fi
-	else
-		got=$(echo "$got" | cut -d' ' -f1)
-		expect=$(echo "$expect" | cut -d' ' -f1)
+    # collect iocccsize -v 0 output
+    #
+    iocccsize_v0=$("$IOCCCSIZE" -v 0 -- "$file" 2>/dev/null)
+    if [[ $V_FLAG -ge 3 ]]; then
+	 echo "$0: debug[3]: $IOCCCSIZE -v 0 $file output: $iocccsize_v0" 1>&2
+    fi
+
+    # collect iocccsize -v 1 output
+    #
+    iocccsize_v1=$("$IOCCCSIZE" -v 1 -- "$file" 2>/dev/null)
+    if [[ $V_FLAG -ge 3 ]]; then
+	 echo "$0: debug[3]: $IOCCCSIZE -v 1 $file output: $iocccsize_v1" 1>&2
+    fi
+
+    # assume all test pass unless we discover otherwise
+    #
+    pass="true"
+
+    # verify iocccsize -v 1 Rule 2a (2nd) value against wc -c
+    #
+    wc_bytes=$(get_wc "$file" 3)
+    iocccsize_v1_2a=$(echo "$iocccsize_v1" | cut -d' ' -f2)
+    if [[ $iocccsize_v1_2a -ne "$wc_bytes" ]]; then
+	echo "$0: ERROR: $IOCCCSIZE -v 1 $file Rule 2a value: $iocccsize_v1_2a != wc -c value: $wc_bytes"
+	EXIT_CODE=1
+	pass="false"
+    elif [[ $V_FLAG -ge 3 ]]; then
+	echo "$0: debug[3]: $IOCCCSIZE -v 1 $file Rule 2a value: $iocccsize_v1_2a == wc -c value: $wc_bytes"
+    fi
+
+    # verify iocccsize -v 1 Rule 2a (2nd) value expected Rule 2a test value
+    #
+    expect_2a=$(echo "$expect" | cut -d' ' -f2)
+    if [[ $iocccsize_v1_2a -ne "$expect_2a" ]]; then
+	echo "$0: ERROR: $IOCCCSIZE -v 1 $file Rule 2a value: $iocccsize_v1_2a != expected Rule 2a value: $expect_2a" 1>&2
+	EXIT_CODE=1
+	pass="false"
+    elif [[ $V_FLAG -ge 3 ]]; then
+	echo "$0: debug[3]: $IOCCCSIZE -v 1 $file Rule 2a value: $iocccsize_v1_2a == expected Rule 2a value: $expect_2a" 1>&2
+    fi
+
+    # verify iocccsize -v 0 (1st) Rule 2b value against expected Rule 2b test value
+    #
+    expect_2b=$(echo "$expect" | cut -d' ' -f1)
+    iocccsize_v0_2b=$(echo "$iocccsize_v0" | cut -d' ' -f1)
+    if [[ $expect_2b -ne "$iocccsize_v0_2b" ]]; then
+	echo "$0: ERROR: $IOCCCSIZE -v 0 $file Rule 2b value: $iocccsize_v0_2b != expected Rule 2b value: $expect_2b" 1>&2
+	EXIT_CODE=1
+	pass="false"
+    elif [[ $V_FLAG -ge 3 ]]; then
+	echo "$0: debug[3]: $IOCCCSIZE -v 0 $file Rule 2b value: $iocccsize_v0_2b == expected Rule 2b value: $expect_2b" 1>&2
+    fi
+
+    # verify iocccsize -v 1 (1st) Rule 2b value against expected Rule 2b test value
+    #
+    expect_2b=$(echo "$expect" | cut -d' ' -f1)
+    iocccsize_v1_2b=$(echo "$iocccsize_v1" | cut -d' ' -f1)
+    if [[ $expect_2b -ne "$iocccsize_v1_2b" ]]; then
+	echo "$0: ERROR: $IOCCCSIZE -v 0 $file Rule 2b value: $iocccsize_v1_2b != expected Rule 2b value: $expect_2b" 1>&2
+	EXIT_CODE=1
+	pass="false"
+    elif [[ $V_FLAG -ge 3 ]]; then
+	echo "$0: debug[3]: $IOCCCSIZE -v 0 $file Rule 2b value: $iocccsize_v1_2b -= expected Rule 2b value: $expect_2b" 1>&2
+    fi
+
+    # report no test failures if debugging
+    #
+    if [[ $pass == "true" ]]; then
+	if [[ $V_FLAG -ge 1 ]]; then
+	    echo "$0: notice: PASS: $file" 1>&2
 	fi
-	if [[ $expect = "$got" ]]; then
-		echo "-OK- $file: $got"
-	else
-		echo "FAIL $file: got $got != expect $expect"
-		EXIT_CODE=1
-	fi
+    else
+	echo "$0: ERROR: FAIL: $file" 1>&2
+    fi
 }
+
+# perform the complete iocccsize test suite
+#
+# tests are of the form:
+#
+#	test_size c_src_file "expected_Rule_2b expected_Rule_2a expected_keywords"
+#
+# NOTE: The expected_keywords (3rd expected arg) is ignored.
 
 #######################################################################
 
-printf 'int x;\r\n' >test_iocccsize/crlf.c
+printf 'int x;\r\n' >"$WORK_DIR/crlf.c"
 test_size crlf.c "2 8 1"
 
 #######################################################################
 
-printf 'char str[] = "\xe8\xe9\xf8";\r\n' >test_iocccsize/utf8.c
+printf 'char str[] = "\xe8\xe9\xf8";\r\n' >"$WORK_DIR/utf8.c"
 test_size utf8.c "12 21 1"
 
 #######################################################################
 
-cat <<EOF >test_iocccsize/splitline0.c
+cat <<EOF >"$WORK_DIR/splitline0.c"
 #define FOO \\
     int a = 666;
 FOO;
@@ -136,7 +266,7 @@ test_size splitline0.c "19 36 1"
 
 #######################################################################
 
-cat <<EOF >test_iocccsize/comment0.c
+cat <<EOF >"$WORK_DIR/comment0.c"
 // comment one line "with a comment string" inside
 int x;
 EOF
@@ -144,7 +274,7 @@ test_size comment0.c "44 58 1"
 
 #######################################################################
 
-cat <<EOF >test_iocccsize/comment1.c
+cat <<EOF >"$WORK_DIR/comment1.c"
 /* comment block same line 'with a comment string' */
 int x;
 EOF
@@ -152,7 +282,7 @@ test_size comment1.c "46 61 1"
 
 #######################################################################
 
-cat <<EOF >test_iocccsize/comment2.c
+cat <<EOF >"$WORK_DIR/comment2.c"
 /*
 comment block
 multiline
@@ -163,35 +293,35 @@ test_size comment2.c "27 37 1"
 
 #######################################################################
 
-cat <<EOF >test_iocccsize/comment3.c
+cat <<EOF >"$WORK_DIR/comment3.c"
 a//foo
 EOF
 test_size comment3.c "6 7 0"
 
 #######################################################################
 
-cat <<EOF >test_iocccsize/comment4.c
+cat <<EOF >"$WORK_DIR/comment4.c"
 /*/ int if for /*/
 EOF
 test_size comment4.c "14 19 0"
 
 #######################################################################
 
-cat <<EOF >test_iocccsize/comment5.c
+cat <<EOF >"$WORK_DIR/comment5.c"
 '"' "/*" foobar "*/"
 EOF
 test_size comment5.c "17 21 0"
 
 #######################################################################
 
-cat <<EOF >test_iocccsize/comment6.c
+cat <<EOF >"$WORK_DIR/comment6.c"
 char str[] = "string /* with */ comment";
 EOF
 test_size comment6.c "30 42 1"
 
 #######################################################################
 
-cat <<EOF >test_iocccsize/comment7.c
+cat <<EOF >"$WORK_DIR/comment7.c"
 // comment with backslash newline \\
 int a = 666;
 EOF
@@ -199,28 +329,28 @@ test_size comment7.c "37 49 0"
 
 #######################################################################
 
-cat <<EOF >test_iocccsize/quote0.c
+cat <<EOF >"$WORK_DIR/quote0.c"
 char str[] = "and\"or";
 EOF
 test_size quote0.c "16 24 1"
 
 #######################################################################
 
-cat <<EOF >test_iocccsize/quote1.c
+cat <<EOF >"$WORK_DIR/quote1.c"
 char squote = '\\'';
 EOF
 test_size quote1.c "12 20 1"
 
 #######################################################################
 
-cat <<EOF >test_iocccsize/quote2.c
+cat <<EOF >"$WORK_DIR/quote2.c"
 char str[] = "'xor'";
 EOF
 test_size quote2.c "14 22 1"
 
 #######################################################################
 
-cat <<EOF >test_iocccsize/digraph.c
+cat <<EOF >"$WORK_DIR/digraph.c"
 char str<::> = "'xor'";
 EOF
 if [[ -z $DIGRAPHS ]]; then
@@ -231,7 +361,7 @@ fi
 
 #######################################################################
 
-cat <<EOF >test_iocccsize/trigraph0.c
+cat <<EOF >"$WORK_DIR/trigraph0.c"
 char str??(??) = "'xor'";
 EOF
 if [[ -z $TRIGRAPHS ]]; then
@@ -243,7 +373,7 @@ fi
 #######################################################################
 
 # Example from https://en.wikipedia.org/wiki/Digraphs_and_trigraphs#C
-cat <<EOF >test_iocccsize/trigraph1.c
+cat <<EOF >"$WORK_DIR/trigraph1.c"
 // Will the next line be executed????????????????/
 int a = 666;
 EOF
@@ -256,7 +386,7 @@ fi
 #######################################################################
 
 # Example from https://en.wikipedia.org/wiki/Digraphs_and_trigraphs#C
-cat <<EOF >test_iocccsize/trigraph2.c
+cat <<EOF >"$WORK_DIR/trigraph2.c"
 /??/
 * A comment *??/
 /
@@ -269,7 +399,7 @@ fi
 
 #######################################################################
 
-cat <<EOF >test_iocccsize/trigraph3.c
+cat <<EOF >"$WORK_DIR/trigraph3.c"
 #define FOO ??/
     int a = 666;
 FOO;
@@ -282,7 +412,7 @@ fi
 
 #######################################################################
 
-cat <<EOF >test_iocccsize/main0.c
+cat <<EOF >"$WORK_DIR/main0.c"
 int
 main(int argc, char **argv)
 {
@@ -293,7 +423,7 @@ test_size main0.c "22 47 4"
 
 #######################################################################
 
-cat <<EOF >test_iocccsize/hello.c
+cat <<EOF >"$WORK_DIR/hello.c"
 #include <stdio.h>
 
 int
@@ -309,7 +439,7 @@ test_size hello.c "58 101 6"
 
 # Digraph for #include and curlys.  Digraphs are tokens and are not
 # translated like trigraphs.
-cat <<EOF >test_iocccsize/hello_digraph.c
+cat <<EOF >"$WORK_DIR/hello_digraph.c"
 %:    include <stdio.h>
 
 int
@@ -329,7 +459,7 @@ fi
 
 # Trigraph for #include and curlys.  Trigraphs are translated, unlike
 # digraphs which are tokens.
-cat <<EOF >test_iocccsize/hello_trigraph.c
+cat <<EOF >"$WORK_DIR/hello_trigraph.c"
 ??=    include <stdio.h>
 
 int
@@ -347,14 +477,14 @@ fi
 
 #######################################################################
 
-cat <<EOF >test_iocccsize/include0.c
+cat <<EOF >"$WORK_DIR/include0.c"
 #  include <stdio.h>
 EOF
 test_size include0.c "10 21 1"
 
 #######################################################################
 
-cat <<EOF >test_iocccsize/include1.c
+cat <<EOF >"$WORK_DIR/include1.c"
 #  include <stdio.h>
 #/*hi*/include <ctype.h>
 EOF
@@ -362,7 +492,7 @@ test_size include1.c "26 46 2"
 
 #######################################################################
 
-cat <<EOF >test_iocccsize/curly0.c
+cat <<EOF >"$WORK_DIR/curly0.c"
 char str = "{ curly } ";
 EOF
 test_size curly0.c "12 25 1"
@@ -370,7 +500,7 @@ test_size curly0.c "12 25 1"
 #######################################################################
 
 # No spaces after curly braces in array initialiser.
-cat <<EOF >test_iocccsize/curly1.c
+cat <<EOF >"$WORK_DIR/curly1.c"
 #include <stdlib.h>
 
 #define STRLEN(s)		(sizeof (s)-1)
@@ -390,7 +520,7 @@ test_size curly1.c "119 192 6"
 #######################################################################
 
 # Spaces after curly braces in array initialiser.
-cat <<EOF >test_iocccsize/curly2.c
+cat <<EOF >"$WORK_DIR/curly2.c"
 #include <stdlib.h>
 
 #define STRLEN(s)		(sizeof (s)-1)
@@ -409,7 +539,7 @@ test_size curly2.c "113 196 6"
 
 #######################################################################
 
-cat <<EOF >test_iocccsize/semicolon0.c
+cat <<EOF >"$WORK_DIR/semicolon0.c"
 char str = "; xor; ";
 EOF
 test_size semicolon0.c "10 22 1"
@@ -417,7 +547,7 @@ test_size semicolon0.c "10 22 1"
 #######################################################################
 
 # Spaces after semicolons in for( ; ; ).
-cat <<EOF >test_iocccsize/semicolon1.c
+cat <<EOF >"$WORK_DIR/semicolon1.c"
 #include <stdio.h>
 
 int
@@ -435,7 +565,7 @@ test_size semicolon1.c "65 133 8"
 #######################################################################
 
 # No spaces after semicolons in for(;;).
-cat <<EOF >test_iocccsize/semicolon2.c
+cat <<EOF >"$WORK_DIR/semicolon2.c"
 #include <stdio.h>
 
 int
@@ -450,7 +580,13 @@ main(int argc, char **argv)
 EOF
 test_size semicolon2.c "67 127 8"
 
-#######################################################################
-# END
-#######################################################################
+# All Done!!! -- Jessica Noll, Age 2
+#
+if [[ $EXIT_CODE -eq 0 ]]; then
+    if [[ $V_FLAG -ge 1 ]]; then
+	echo "$0: notice: all tests PASSED"
+    fi
+else
+    echo "$0: ERROR: about to exit $EXIT_CODE" 1>&2
+fi
 exit "$EXIT_CODE"
