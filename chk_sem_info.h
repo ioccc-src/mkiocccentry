cca3ba547915c16630414687139a291c62d8c1ad
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2022-10-27 05:44:59 -0700

    Comments consistency update
    Specifically with the JSON related files I added the text 'This JSON
    parser was co-developed by ...' in files that were missing it. There
    might still be some that are missing it of course but it's been
    addressed in some now.

--- chk_sem_info.h
+++ chk_sem_info.h
@@ -3,6 +3,16 @@
  *
  * "Because grammar and syntax alone do not make a complete language." :-)
  *
+ * This JSON parser was co-developed by:
+ *
+ *	@xexyl
+ *	https://xexyl.net		Cody Boone Ferguson
+ *	https://ioccc.xexyl.net
+ * and:
+ *	chongo (Landon Curt Noll, http://www.isthe.com/chongo/index.html) /\oo/\
+ *
+ * "Because sometimes even the IOCCC Judges need some help." :-)
+ *
  * The concept of this file was developed by:
  *
  *	chongo (Landon Curt Noll, http://www.isthe.com/chongo/index.html) /\oo/\
