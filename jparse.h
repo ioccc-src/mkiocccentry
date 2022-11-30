9f88164a2efebf03b132b457bd6e962b49793baa
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2022-11-30 10:01:01 -0800

    Typo fix in jparse.h

--- jparse.h
+++ jparse.h
@@ -3,7 +3,7 @@
  *
  * "Because specs w/o version numbers are forced to commit to their original design flaws." :-)
  *
- * This JSON scanner and parser was co-developed by:
+ * This JSON parser was co-developed by:
  *
  *	@xexyl
  *	https://xexyl.net		Cody Boone Ferguson
