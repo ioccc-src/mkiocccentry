e59f15db96dc0893341a985825fbb6028525a278
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2023-01-13 03:56:47 -0800

    Add function sem_member_value_size_t()
    This will be important in a future commit when addressing sign
    conversion warnings (-Wsign-conversion). It's possible that other
    functions of this kind should also be made for the rest of the
    json_number types that have no counterpart function but this can be
    determined later.

--- jparse/json_sem.h
+++ jparse/json_sem.h
@@ -148,6 +148,9 @@ extern struct str_or_null sem_member_value_str_or_null(struct json const *node, 
 						       char const *name, struct json_sem_val_err **val_err);
 extern int *sem_member_value_int(struct json const *node, unsigned int depth, struct json_sem *sem,
 			         char const *name, struct json_sem_val_err **val_err);
+
+extern size_t *sem_member_value_size_t(struct json const *node, unsigned int depth, struct json_sem *sem,
+			         char const *name, struct json_sem_val_err **val_err);
 extern time_t *sem_member_value_time_t(struct json const *node, unsigned int depth, struct json_sem *sem,
 				       char const *name, struct json_sem_val_err **val_err);
 extern struct json *sem_node_parent(struct json const *node, unsigned int depth, struct json_sem *sem,
