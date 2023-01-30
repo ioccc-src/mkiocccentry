6fd117ac670974229f7f5c1d742fc6c14a81d54b
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2023-01-30 10:00:30 -0800

    Check remaining script files for error messages
    With the exceptions noted on GitHub all error messages should be
    checked.
    
    I did not check these as thoroughly as in the C files (and I very
    possibly missed some there too) but I did discover a number of issues
    that were fixed (or else noted on GitHub for discussion).

--- test_ioccc/mkiocccentry_test.sh
+++ test_ioccc/mkiocccentry_test.sh
@@ -254,7 +254,7 @@ if [[ ${status} -ne 0 ]]; then
 fi
 rm -f "${src_dir}"/empty.c
 
-# Form entries that are unlikely to win the IOCCC :-)
+# Form entries that are (un?)likely to win the IOCCC :-)
 #
 test -f "${src_dir}"/prog.c || {
 cat >"${src_dir}"/prog.c <<"EOF"
