2f9c0fb5a5df6ffb9144beccffb53852ad96c8a3
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2023-05-25 03:35:52 -0700

    Initial jprint code
    Right now it only links in the jparse, dynamic array and debug
    libraries and when run it exits 0.
    
    The Makefile has been updated and make depend was run as well.
    
    This tool will I believe take some time and careful thought and it
    definitely will be developed over many commits but now it should be
    possible to just start writing the code without having to worry about
    editing the Makefile etc. More editing of the Makefile will have to be
    done later - for instance when man pages are added or some dependency
    changes or if a new file has to be added but at this point I can start
    working on the code once I have a better idea of what is needed. This
    won't happen until a discussion is had on GitHub though I might be able
    to get some code in prior to that should I find the time and energy. I
    do hope to do a bit more on this later today.
    
    Updated .gitignore: add jparse/jprint.

---
+++ jparse/jprint.h
@@ -0,0 +1,34 @@
+/*
+ * jprint - JSON parser printer
+ *
+ * "Because specs w/o version numbers are forced to commit to their original design flaws." :-)
+ *
+ * This tool is being developed by:
+ *
+ *	@xexyl
+ *	https://xexyl.net		Cody Boone Ferguson
+ *	https://ioccc.xexyl.net
+ *
+ * The JSON parser was co-developed in 2022 by:
+ *
+ *	Cody Boone Ferguson
+ * and:
+ *	chongo (Landon Curt Noll, http://www.isthe.com/chongo/index.html) /\oo/\
+ *
+ * "Because sometimes even the IOCCC Judges need some help." :-)
+ *
+ * "Share and Enjoy!"
+ *     --  Sirius Cybernetics Corporation Complaints Division, JSON spec department. :-)
+ */
+
+
+#if !defined(INCLUDE_JPARSE_H)
+#    define  INCLUDE_JPARSE_H
+
+#include <stdlib.h>
+
+#include "jparse.h"
+
+#include "util.h"
+
+#endif
