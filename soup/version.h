67195e8ffd05dbbda86b87cef7712037f2589074
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2024-08-23 07:01:35 -0700

    Add a number of test files to test_JSON - #931
    Added a number of test JSON files to demonstrate that the JSON parser is
    okay with mixed spacing (spaces and tabs or different number of spaces
    with or without tabs etc.), no spacing and so on, even though the
    mkiocccentry(1) tool will form json files with multiples of 4 spaces and
    no tabs.
    
    Updated MKIOCCCENTRY_VERSION to "1.0.8 2024-08-23".

--- soup/version.h
+++ soup/version.h
@@ -81,7 +81,7 @@
 /*
  * official mkiocccentry version
  */
-#define MKIOCCCENTRY_VERSION "1.0.7 2024-07-12"	/* format: major.minor YYYY-MM-DD */
+#define MKIOCCCENTRY_VERSION "1.0.8 2024-08-23"	/* format: major.minor YYYY-MM-DD */
 
 /*
  * Version of info for JSON the .info.json file.
