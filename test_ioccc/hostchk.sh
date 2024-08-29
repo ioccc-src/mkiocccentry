1434b45aba52d32e593a1963d56a15376e403543
Landon Curt Noll <github-mail@asthe.com>
2024-08-28 21:18:05 -0700

    fix test_ioccc/hostchk.sh
    Do not allow `test_ioccc/hostchk.sh` to test _<dyn_array.h>_,
    nor _<dbg.h>_.
    
    Compile with -DMKIOCCCENTRY_SRC as per what `Makefile` does.

--- test_ioccc/hostchk.sh
+++ test_ioccc/hostchk.sh
@@ -238,7 +238,7 @@ if [[ -n $F_FLAG ]]; then
     printf "%s\\n%s\\n" "$(grep '#include.*<.*>' "$TOPDIR"/*.[hc] "$TOPDIR"/dbg/*.[hc] \
 	"$TOPDIR"/dyn_array/*.[hc] "$TOPDIR"/test_ioccc/*.[ch] "$TOPDIR"/soup/*.[hc] "$TOPDIR"/jparse/*.[hcly] \
 	"$TOPDIR"/jparse/test_jparse/*.[hc] |cut -f 2- -d:|sort -u)" "int main(void) { return 0; }" |
-	    "${CC}" -x c - -o "$PROG_FILE"
+	    "${CC}" -DMKIOCCCENTRY_SRC -x c - -o "$PROG_FILE"
     status="$?"
     if [[ $status -ne 0 ]]; then
 	EXIT_CODE=14	# will exit 14 at the end unless EXIT_CODE is changed later on
@@ -274,6 +274,8 @@ elif [[ -n $RUN_INCLUDE_TEST ]]; then
     #
     grep '#include.*<.*>' "$TOPDIR"/*.[hc] "$TOPDIR"/dbg/*.[hc] "$TOPDIR"/dyn_array/*.[hc] \
         "$TOPDIR"/test_ioccc/*.[ch] "$TOPDIR"/soup/*.[hc] "$TOPDIR"/jparse/*.[hcly] "$TOPDIR"/jparse/test_jparse/*.[hc] | \
+	grep -F -v '<dyn_array.h>' |
+	grep -F -v '<dbg.h>' |
 	cut -f 2- -d:|sort -u| while read -r h; do
 
 	# form C prog
@@ -282,7 +284,7 @@ elif [[ -n $RUN_INCLUDE_TEST ]]; then
 	    echo "Compiling code with \"$h\"" 1>&2
 	fi
 	rm -f "$PROG_FILE"
-	printf "%s\\n%s\\n" "$h" "int main(void){ return 0; }" | "${CC}" -x c - -o "$PROG_FILE"
+	printf "%s\\n%s\\n" "$h" "int main(void){ return 0; }" | "${CC}" -DMKIOCCCENTRY_SRC -x c - -o "$PROG_FILE"
 
 	# test compile
 	#
