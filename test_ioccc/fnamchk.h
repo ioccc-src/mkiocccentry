e2e5d108bee2e88f192739ae6d2baad03daa1791
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2025-01-21 05:58:35 -0800

    Typo fix and update comments in fnamchk

--- test_ioccc/fnamchk.h
+++ test_ioccc/fnamchk.h
@@ -3,33 +3,21 @@
  *
  * "Because even fprintf has a return value worth paying attention to." :-)
  *
- * Copyright (c) 2022 by Landon Curt Noll.  All Rights Reserved.
- *
- * Permission to use, copy, modify, and distribute this software and
- * its documentation for any purpose and without fee is hereby granted,
- * provided that the above copyright, this permission notice and text
- * this comment, and the disclaimer below appear in all of the following:
- *
- *       supporting documentation
- *       source copies
- *       source works derived from this source
- *       binaries derived from this source or from derived source
- *
- * LANDON CURT NOLL DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
- * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO
- * EVENT SHALL LANDON CURT NOLL BE LIABLE FOR ANY SPECIAL, INDIRECT OR
- * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF
- * USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
- * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
- * PERFORMANCE OF THIS SOFTWARE.
- *
- * chongo (Landon Curt Noll, http://www.isthe.com/chongo/index.html) /\oo/\
- *
  * "Because everyone hates when someone gets their name wrong." :-)
+ *
+ * This tool was co-developed in 2022 by Cody Boone Ferguson and Landon Curt
+ * Noll:
+ *
+ *	@xexyl
+ *	https://xexyl.net		Cody Boone Ferguson
+ *	https://ioccc.xexyl.net
+ * and:
+ *	chongo (Landon Curt Noll, http://www.isthe.com/chongo/index.html) /\oo/\
+ *
+ * "Because sometimes even the IOCCC Judges need some help." :-)
  *
  * Share and enjoy! :-)
  */
-
 
 #if !defined(INCLUDE_FNAMCHK_H)
 #    define  INCLUDE_FNAMCHK_H
